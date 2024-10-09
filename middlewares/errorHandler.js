5caacc69be38eb8cbbc6e18cfe5d0c8763e77c3f
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-09 14:54:29 +0700

    No modification

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,7 @@ function errorHandler(handler, useNext=false) {
     return async function(req, res, next) {
         try {
             useNext ? await handler(req, res, next) : await handler(req, res);
-        } catch (error) {
+        } catch(error) {
             console.error(error);
             res.json({status: 400, msg: 'Unknown dependency error'});
         }
