25d407709f9608214a0a2003812a06fb6ee61794
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-10 16:32:31 +0700

    Register and validate function modified

--- controllers/register.js
+++ controllers/register.js
@@ -1,13 +1,34 @@
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
+const TransactionError = require('../services/TransactionError');
+const sendMail = require('../services/mailService');
+const User = require('../models/User');
 const UserTemp = require('../models/UserTemp');
 
 async function register(req, res) {
-    const User = new UserTemp();
     const otp = generateOtp();
-    await User.register(req.data, otp);
-    await sendOtpToEmail(req.data.email, otp);
-    res.json({status: 202, msg: 'Registration is in process. Please check your email for the OTP'});
+    const UserTempVar = new UserTemp();
+    await UserTempVar.register(req.data, otp);
+
+    sendMail('verification', {to: req.data.email, otp});
+
+    const data = {email: req.data.email};
+    const token = generateToken(data, 15 * 60);
+
+    res.json({status: 202, msg: 'Registration is in process. Please check your email for the OTP', token});
+}
+
+async function validate(req, res) {
+    const user = await UserTemp.findOneAndDelete({email: req.data.email}, {session: req.mongooseSession});
+    if (!user) throw new TransactionError({status: 404, msg: 'Validation failed. Email not found'});
+
+    const {name, email, pwd, otp} = user;
+    if (otp !== req.data.otp) throw new TransactionError({status: 401, msg: 'Validation failed. OTP is incorrect'});
+
+    await User.create([{name, email, pwd}], {session: req.mongooseSession});
+
+    res.json({status: 201, msg: 'Registration succeeded. Email has been validated'});
 }
 
 function generateOtp() {
@@ -15,10 +36,8 @@ function generateOtp() {
     return otp.toString();
 }
 
-async function sendOtpToEmail(email, otp) {
-    console.log(`Send otp ${otp} to ${email}`);
+function generateToken(info, expiryTime) {
+    return jwt.sign(info, process.env.JWT_SECRET, {expiresIn: expiryTime});
 }
 
-module.exports = {
-    register
-};
+module.exports = {register, validate};
