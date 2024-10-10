b0148a3ff604e3de381ae84686c69cae924f698d
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-10 16:31:52 +0700

    Middlewares and endpoints using errorhandler and transactionhandler

--- routes/register.js
+++ routes/register.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+const errorHandler = require('../middlewares/errorHandler');
+const transactionHandler = require('../middlewares/transactionHandler');
+
 const validation = require('../validations/register');
 const controller = require('../controllers/register');
 
 const routes = express.Router();
 
-routes.post('/', validation.register, controller.register); // Register User
-routes.post('/validate') // Validate User
+routes.post('/', errorHandler(validation.register), errorHandler(controller.register));
+routes.post('/validate', errorHandler(validation.validate), transactionHandler(controller.validate));
 
 module.exports = routes;
