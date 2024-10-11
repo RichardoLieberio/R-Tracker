7f6a2b6408a8b34a0faad40d4313b9be0d817198
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-11 09:56:28 +0700

    Token routes added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require('./services/dbConnection');
 
 connectDB();
 
+const tokenRoutes = require('./routes/token');
 const registerRoutes = require('./routes/register');
 const userRoutes = require('./routes/user');
 
@@ -11,8 +12,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/register/', registerRoutes);
-app.use('/api/user/', userRoutes);
+app.use('/api/token', tokenRoutes);
+app.use('/api/register', registerRoutes);
+app.use('/api/user', userRoutes);
 
 app.listen(process.env.PORT, function() {
     console.log(`Listening on port ${process.env.PORT}`);
