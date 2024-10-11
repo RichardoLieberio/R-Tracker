073c0de2c39ac745abd3951a144ed99eb23ce011
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-11 11:21:35 +0700

    Modify token validation

--- middlewares/logoutRequired.js
+++ middlewares/logoutRequired.js
@@ -1,8 +1,17 @@
-function logoutRequired(req, res, next) {
-    const refreshToken = req.cookies[process.env.REFRESH_TOKEN_COOKIE];
-    const accessToken = req.headers['authorization']?.split(' ')[1];
+const jwt = require('jsonwebtoken');
 
-    if (refreshToken || accessToken) return res.json({status: 401, msg: 'You need to log out first to proceed'});
+function logoutRequired(req, res, next) {
+    const accessToken = req.headers['authorization']?.split(' ')[1];
+    const refreshToken = req.cookies[process.env.REFRESH_TOKEN_COOKIE];
+
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, function(error) {
+        if (!error) return res.json({status: 401, msg: 'You need to log out first to proceed'});
+    })
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, function(error) {
+        if (!error) return res.json({status: 401, msg: 'You need to log out first to proceed'});
+    })
+
     next();
 }
 
