46f854b7f4705252f427a7484612e3a0a1be11b8
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-11 11:33:21 +0700

    Logout Required bug fixed

--- middlewares/logoutRequired.js
+++ middlewares/logoutRequired.js
@@ -1,18 +1,24 @@
 const jwt = require('jsonwebtoken');
 
-function logoutRequired(req, res, next) {
+async function logoutRequired(req, res, next) {
     const accessToken = req.headers['authorization']?.split(' ')[1];
     const refreshToken = req.cookies[process.env.REFRESH_TOKEN_COOKIE];
 
-    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, function(error) {
-        if (!error) return res.json({status: 401, msg: 'You need to log out first to proceed'});
-    })
+    try {
+        await verifyToken(accessToken, process.env.ACCESS_TOKEN_SECRET);
+        await verifyToken(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        next();
+    } catch(error) {
+        res.json({status: 401, msg: 'You need to log out first to proceed'});
+    }
+}
 
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, function(error) {
-        if (!error) return res.json({status: 401, msg: 'You need to log out first to proceed'});
-    })
-
-    next();
+function verifyToken(token, secret) {
+    return new Promise(function(resolve, reject) {
+        jwt.verify(token, secret, function(error) {
+            return error ? resolve() : reject();
+        })
+    });
 }
 
 module.exports = logoutRequired;
