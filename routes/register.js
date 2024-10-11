17e54e46843b24b89848e07f5210090e4d60a5cd
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-11 11:21:56 +0700

    Register and validate requires logout

--- routes/register.js
+++ routes/register.js
@@ -4,13 +4,14 @@ const csrfProtection = require('../services/csrfProtection');
 const csrfHandler = require('../middlewares/csrfHandler');
 const errorHandler = require('../middlewares/errorHandler');
 const transactionHandler = require('../middlewares/transactionHandler');
+const logoutRequired = require('../middlewares/logoutRequired');
 
 const validation = require('../validations/register');
 const controller = require('../controllers/register');
 
 const routes = express.Router();
 
-routes.post('/', csrfHandler(csrfProtection), errorHandler(validation.register, true), errorHandler(controller.register));
-routes.post('/validate', csrfHandler(csrfProtection), errorHandler(validation.validate, true), transactionHandler(controller.validate));
+routes.post('/', logoutRequired, csrfHandler(csrfProtection), errorHandler(validation.register, true), errorHandler(controller.register));
+routes.post('/validate', logoutRequired, csrfHandler(csrfProtection), errorHandler(validation.validate, true), transactionHandler(controller.validate));
 
 module.exports = routes;
