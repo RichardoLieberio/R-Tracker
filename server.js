04c44e03ef9f822f0b4c61b2c6b68eb1f29322ce
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-11 10:35:25 +0700

    Cookie parser and csrf dependencies added

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const connectDB = require('./services/dbConnection');
-
-connectDB();
-
 const tokenRoutes = require('./routes/token');
 const registerRoutes = require('./routes/register');
 const userRoutes = require('./routes/user');
 
+connectDB();
+
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api/token', tokenRoutes);
