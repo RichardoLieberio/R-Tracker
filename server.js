f73356b1205afc4d4e0dc692134dfe5ac8041fba
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-11 08:23:21 +0700

    User api routes created

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const connectDB = require('./services/dbConnection');
 connectDB();
 
 const registerRoutes = require('./routes/register');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/register/', registerRoutes);
+app.use('/api/user/', userRoutes);
 
 app.listen(process.env.PORT, function() {
     console.log(`Listening on port ${process.env.PORT}`);
