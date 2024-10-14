2c547ec3aaedc922c11e347e35750c7ef754f52a
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-14 14:57:38 +0700

    Create mail options for password reset

--- services/mailService.js
+++ services/mailService.js
@@ -15,7 +15,12 @@ function sendMail(type, mail) {
     switch (type) {
         case 'verification':
             transporter.sendMail(createVerificationOptions(mail.to, mail.otp), function(error) {
-                error ? console.log(`Failed to send verification email to ${mail.to} ${error}`) : console.log(`Sucessfully sent verification email to ${mail.to}`);
+                error ? console.log(`Failed to send verification email to ${mail.to}`) : console.log(`Sucessfully sent verification email to ${mail.to}`);
+            });
+            break;
+        case 'pwd-reset':
+            transporter.sendMail(createPwdResetOptions(mail.to, mail.uri), function(error) {
+                error ? console.log(`Failed to send password reset email to ${mail.to}`) : console.log(`Sucessfully sent password reset email to ${mail.to}`);
             });
             break;
         default:
@@ -34,6 +39,26 @@ function createVerificationOptions(to, otp) {
             <p>Please use this code below to verify your account.</p>
             <h2><strong>${otp}</strong></h2>
             <p>Code will be valid for <b>15 minutes</b></p>
+            <br>
+            <p>If you didn't request this, please ignore this email</p>
+            <br>
+            <p>Regards,<br>
+            R-Tracker</p>
+        `
+    };
+}
+
+function createPwdResetOptions(to, uri) {
+    return {
+        from: process.env.EMAIL_ADDRESS,
+        to,
+        subject: 'R-Tracker Reset Password',
+        html: `
+            <h1>R-Tracker</h1>
+            <p>We received a request to reset the password for your account.</p>
+            <br>
+            <p>To reset your password, please follow the link below. Link will be valid for <b>15 minutes</b></p>
+            <p><a href="${uri}">${uri}</a></p>
             <br>
             <p>If you didn't request this, please ignore this email</p>
             <br>
