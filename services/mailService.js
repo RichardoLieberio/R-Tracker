fea0db954e5bb61be8a2c85a51a6330580af092b
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-15 13:15:26 +0700

    Updated mail transpoter

--- services/mailService.js
+++ services/mailService.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.EMAIL_ADDRESS,
         pass: process.env.EMAIL_PWD
