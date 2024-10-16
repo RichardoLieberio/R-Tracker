9cee5b567045d863b017ba358dc1a859eb70c19e
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-16 16:16:14 +0700

    Welcoming email template created

--- services/mailService.js
+++ services/mailService.js
@@ -13,8 +13,13 @@ const transporter = nodemailer.createTransport({
 function sendMail(type, mail) {
     switch (type) {
         case 'account-verification':
-            transporter.sendMail(createAccountVerificationOptions(mail.to, mail.otp), function(error) {
+            transporter.sendMail(createAccountVerificationOptions(mail.to, mail.name, mail.otp), function(error) {
                 error ? console.log(`Failed to send verification email to ${mail.to}`) : console.log(`Sucessfully sent verification email to ${mail.to}`);
+            });
+            break;
+        case 'account-verified':
+            transporter.sendMail(createAccountVerifiedOptions(mail.to, mail.name), function(error) {
+                error ? console.log(`Failed to send welcoming email to ${mail.to}`) : console.log(`Sucessfully sent welcoming email to ${mail.to}`);
             });
             break;
         case 'pwd-reset':
@@ -28,7 +33,7 @@ function sendMail(type, mail) {
     }
 }
 
-function createAccountVerificationOptions(to, otp) {
+function createAccountVerificationOptions(to, name, otp) {
     return {
         from: `"${process.env.EMAIL_NAME}" <${process.env.EMAIL_ADDRESS}>`,
         to,
@@ -37,14 +42,37 @@ function createAccountVerificationOptions(to, otp) {
             <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
                 <div style="margin:50px auto;width:70%;padding:20px 0">
                     <div style="border-bottom:1px solid #eee">
-                        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">R-Tracker</a>
+                        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">${process.env.EMAIL_NAME}</a>
                     </div>
-                    <p style="font-size:1.1em">Hi,</p>
-                    <p>Thank you for choosing R-Tracker. Use the following OTP to complete your Sign Up procedures. OTP will be valid for <b>15 minutes</b></p>
+                    <p style="font-size:1.1em">Hi, ${name}</p>
+                    <p>Thank you for choosing ${process.env.EMAIL_NAME}. Use the following OTP to complete your Sign Up procedures. OTP will be valid for <b>15 minutes</b></p>
                     <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
                     <p>If you didn't request this, please ignore this email</p>
                     <hr style="border:none;border-top:1px solid #eee" />
-                    <p style="font-size:0.9em;">Regards,<br />R-Tracker</p>
+                    <p style="font-size:0.9em;">Regards,<br />${process.env.EMAIL_NAME}</p>
+                </div>
+            </div>
+        `
+    };
+}
+
+function createAccountVerifiedOptions(to, name) {
+    return {
+        from: `"${process.env.EMAIL_NAME}" <${process.env.EMAIL_ADDRESS}>`,
+        to,
+        subject: 'Account Successfully Verified',
+        html: `
+            <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+                <div style="margin:50px auto;width:70%;padding:20px 0">
+                    <div style="border-bottom:1px solid #eee">
+                        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">${process.env.EMAIL_NAME}</a>
+                    </div>
+                    <p style="font-size:1.1em">Hi, ${name}</p>
+                    <p>We’re excited to let you know that your account has been successfully verified!</p>
+                    <p>You can now enjoy all the features and services ${process.env.EMAIL_NAME} has to offer</p>
+                    <p>Thank you for being part of ${process.env.EMAIL_NAME}!</p>
+                    <hr style="border:none;border-top:1px solid #eee" />
+                    <p style="font-size:0.9em;">Regards,<br />${process.env.EMAIL_NAME}</p>
                 </div>
             </div>
         `
@@ -60,13 +88,13 @@ function createPwdResetOptions(to, otp) {
             <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
                 <div style="margin:50px auto;width:70%;padding:20px 0">
                     <div style="border-bottom:1px solid #eee">
-                        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">R-Tracker</a>
+                        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">${process.env.EMAIL_NAME}</a>
                     </div>
                     <p>We received a request to reset the password for your account. Please use the code below to continue. OTP will be valid for <b>15 minutes</b></p>
                     <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
                     <p>If you didn't request this, please ignore this email</p>
                     <hr style="border:none;border-top:1px solid #eee" />
-                    <p style="font-size:0.9em;">Regards,<br />R-Tracker</p>
+                    <p style="font-size:0.9em;">Regards,<br />${process.env.EMAIL_NAME}</p>
                 </div>
             </div>
         `
