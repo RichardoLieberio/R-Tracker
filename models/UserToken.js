604070638f018bb9111358bfe4715e281a8884e2
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-18 09:06:55 +0700

    TokenBlacklist removed and UserToken added logout static method

--- models/UserToken.js
+++ models/UserToken.js
@@ -29,8 +29,16 @@ const userTokenSchema = mongoose.Schema({
     }
 });
 
+userTokenSchema.statics.isAuthenticationMatches = async function(user_id, token) {
+    return !!await this.findOne({user_id, $or: [{access_token: token}, {refresh_token: token}]});
+}
+
 userTokenSchema.statics.login = async function(user_id, refresh_token, access_token) {
     await this.findOneAndUpdate({user_id}, {refresh_token, access_token}, {upsert: true});
+}
+
+userTokenSchema.statics.logout = async function(user_id) {
+    await this.findOneAndUpdate({user_id}, {refresh_token: '', access_token: ''}, {upsert: true});
 }
 
 const UserToken = mongoose.model('UserToken', userTokenSchema);
