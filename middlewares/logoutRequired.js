e26823d3bb6f898334ee8c9ddfc7cf5a06a7b2fe
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-18 10:29:34 +0700

    Removed local verifyToken function

--- middlewares/logoutRequired.js
+++ middlewares/logoutRequired.js
@@ -1,27 +1,13 @@
-const jwt = require('jsonwebtoken');
-
-const TokenBlacklist = require('../models/TokenBlacklist');
+const verifyToken = require('../services/verifyToken');
 
 async function logoutRequired(req, res, next) {
     const accessToken = req.headers['authorization']?.split(' ')[1];
     const refreshToken = req.cookies[process.env.REFRESH_TOKEN_COOKIE];
 
-    try {
-        await verifyToken(accessToken, process.env.ACCESS_TOKEN_SECRET);
-        await verifyToken(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-        next();
-    } catch(error) {
-        res.json({status: 401, msg: 'You need to log out first to proceed'});
-    }
-}
+    if (await verifyToken(accessToken, process.env.ACCESS_TOKEN_SECRET) || await verifyToken(refreshToken, process.env.REFRESH_TOKEN_SECRET))
+    return res.json({status: 401, msg: 'You need to log out first to proceed'});
 
-function verifyToken(token, secret) {
-    return new Promise(function(resolve, reject) {
-        jwt.verify(token, secret, async function(error) {
-            if (error) return resolve();
-            await TokenBlacklist.isTokenBlacklisted(token) ? resolve() : reject();
-        });
-    });
+    next();
 }
 
 module.exports = logoutRequired;
