b02432887060bec9d7edd555b5408decb7dbe3ea
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-18 10:30:07 +0700

    Uses verifyToken services

--- middlewares/authRequired.js
+++ middlewares/authRequired.js
@@ -1,26 +1,20 @@
-const jwt = require('jsonwebtoken');
-
+const verifyToken = require('../services/verifyToken');
 const generateAccessToken = require('../services/generateAccessToken');
 
-function authRequired(req, res, next) {
-    try {
-        const accessToken = req.headers['authorization']?.split(' ')[1];
-        req.user = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-        next();
-    } catch(error) {
-        return generateNewAccessToken(req, res);
-    }
-}
+const UserToken = require('../models/UserToken');
 
-function generateNewAccessToken(req, res) {
-    try {
-        const refreshToken = req.cookies[process.env.REFRESH_TOKEN_COOKIE];
-        const {id, role} = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-        const accessToken = generateAccessToken({id, role});
-        return res.json({status: 200, msg: 'New access token generated successfully', accessToken});
-    } catch(error) {
-        return res.json({status: 401, msg: 'Invalid or expired refresh token'});
-    }
+async function authRequired(req, res, next) {
+    const accessToken = req.headers['authorization']?.split(' ')[1];
+    req.userId = await verifyToken(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    if (req.userId) return next();
+
+    const refreshToken = req.cookies[process.env.REFRESH_TOKEN_COOKIE];
+    req.userId = await verifyToken(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    if (!req.userId) return res.json({status: 401, msg: 'Invalid or expired refresh token'});
+
+    const newAccessToken = generateAccessToken({id: req.userId});
+    await UserToken.resetAccessToken(req.userId, newAccessToken);
+    res.json({status: 200, msg: 'New access token generated successfully', accessToken: newAccessToken});
 }
 
 module.exports = authRequired;
