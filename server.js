9bba59bd5e9efc82a0ff20331265b353fad015cf
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-21 13:35:37 +0700

    CORS setup and accept limited json size

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const connectDB = require('./services/dbConnection');
+const sizeLimiter = require('./middlewares/sizeLimiter');
 
 const tokenRoutes = require('./routes/token');
 const requestRoutes = require('./routes/request');
@@ -14,8 +16,14 @@ connectDB();
 
 const app = express();
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(cookieParser());
-app.use(express.json());
+app.use(sizeLimiter);
+app.use(express.json({limit: `${process.env.ACCEPT_JSON_MAX_SIZE_IN_MB}mb`}));
 
 app.use('/api/token', tokenRoutes);
 app.use('/api/request', requestRoutes);
