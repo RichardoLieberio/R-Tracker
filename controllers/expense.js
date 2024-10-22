8d8a6300aba24b4054a7eb9d8a5a0d129143721f
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-22 10:55:01 +0700

    Change getCategories to getCategoriesForUser

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,7 +1,7 @@
 const ExpenseCategory = require('../models/ExpenseCategory');
 
 async function getCategories(req, res) {
-    const categories = await ExpenseCategory.getCategories();
+    const categories = await ExpenseCategory.getCategoriesForUser();
     res.json({status: 200, msg: 'Categories retrieved successfully', categories});
 }
 
