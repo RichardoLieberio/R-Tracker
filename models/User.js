993dc22ff8c3ec8a69cc35f07bdb22c7ff8849db
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-23 10:07:59 +0700

    Added static method to whitelist user

--- models/User.js
+++ models/User.js
@@ -101,6 +101,10 @@ userSchema.statics.changePwdByAdmin = async function(_id, rawPwd, session) {
     return !!await this.findOneAndUpdate({_id}, {pwd, updated_at: Date.now()}, {session});
 }
 
+userSchema.statics.whitelist = async function(_id) {
+    return !!await this.findOneAndUpdate({_id}, {$unset: {blacklisted: '', blacklist_reason: '', blacklisted_by: '', blacklisted_at: ''}});
+}
+
 userSchema.statics.blacklist = async function(_id, blacklist_reason, blacklisted_by, session) {
     return !!await this.findOneAndUpdate({_id}, {blacklisted: true, blacklist_reason, blacklisted_by, blacklisted_at: Date.now()}, {session});
 }
