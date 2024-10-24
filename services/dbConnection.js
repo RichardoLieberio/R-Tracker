f2be76f9e43882ce144350bd9306cd18b2cd874f
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-24 09:33:45 +0700

    Nothing changes

--- services/dbConnection.js
+++ services/dbConnection.js
@@ -4,7 +4,7 @@ async function connectDB() {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log('Database connected');
-    } catch (error) {
+    } catch(error) {
         console.error(`Database connection failed: ${error}`);
         process.exit(1);
     }
