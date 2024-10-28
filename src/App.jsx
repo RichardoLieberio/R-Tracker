8f5be0ed4369376b0b64a9f1b2c409f283d7955e
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-28 11:39:26 +0700

    Routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ChangeEmail from './pages/ChangeEmail';
+import ResetPwd from './pages/ResetPwd';
+import Dashboard from './pages/Dashboard';
+import Expense from './pages/Expense';
+import User from './pages/User';
+import ExpenseCategory from './pages/ExpenseCategory';
+
 export default function App() {
     return (
-        <h1 className="text-red-500">Hello World</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/change-email" element={<ChangeEmail />} />
+                <Route path="/reset-password" element={<ResetPwd />} />
+                <Route path="/expense" element={<Expense />} />
+                <Route path="/admin">
+                    <Route path="user" element={<User />} />
+                    <Route path="expense-category" element={<ExpenseCategory />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
