87da2bb528b8bb06470305a1a428516f0b8a05ac
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-29 10:30:17 +0700

    Added min width for main layout

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -5,7 +5,7 @@ export default function MainLayout({children}) {
     const theme = useSelector((state) => state.theme.color);
 
     return (
-        <main className="w-full h-screen" style={{backgroundColor: theme.background}}>
+        <main className="w-full min-w-60 h-screen relative" style={{backgroundColor: theme.background}}>
             {children}
         </main>
     );
