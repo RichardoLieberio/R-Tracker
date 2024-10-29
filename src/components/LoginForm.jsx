bd0bd1f67839d8d9fda911937ddf406b03fae949
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-29 12:06:47 +0700

    Login page's image working

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,20 +1,33 @@
 import {useMediaQuery} from '@mui/material';
 
 import breakpoints from '../../config/breakpoints';
+import themeConfig from '../../config/theme';
 
 export default function LoginForm() {
     const tabletBreakpoint = useMediaQuery(`(min-width: ${breakpoints.tablet})`);
 
     return (
-        <form className="flex absolute right-1/2 top-1/2 translate-x-1/2 translate-y-1/2 bg-red-300">
+        <form className="flex absolute right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2 bg-red-300" style={{boxShadow: `0 0 4px ${themeConfig.purple.shadow}`}}>
             {
                 tabletBreakpoint &&
-                <section className="">
-
-                </section>
+                <section className="w-56 h-auto phone:w-60 tablet:w-80 desktop:w-96 bg-cover bg-center bg-no-repeat" style={{backgroundImage: 'url("/Login Image.jpeg")'}} />
             }
-            <section className="">
+            <section className="w-56 phone:w-60 tablet:w-80 desktop:w-96">
                 <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <h1>Login form</h1>
+                <button style={{color: themeConfig.purple.oppositeText, backgroundColor: themeConfig.purple.primary}}>Login</button>
             </section>
         </form>
     );
