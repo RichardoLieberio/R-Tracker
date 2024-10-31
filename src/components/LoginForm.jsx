7c29afbb09a8aec30e4fe0604eb5c04faad02979
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-31 11:18:01 +0700

    Login page and form done

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,46 +1,83 @@
+import {useState, useRef} from 'react';
 import {useMediaQuery} from '@mui/material';
 
 import breakpoints from '../../config/breakpoints';
-import themeConfig from '../../config/theme';
-
-import getClassName from '../services/getClassName';
 
 export default function LoginForm() {
+    const [email, setEmail] = useState('');
+    const [pwd, setPwd] = useState('');
+
+    const emailLabelRef = useRef(null);
+    const pwdLabelRef = useRef(null);
+
     const tabletBreakpoint = useMediaQuery(`(min-width: ${breakpoints.tablet})`);
 
-    const border = getClassName('border', 'purple', 'primary');
-    console.log(border);
+    function emailHandler(e) {
+        setEmail(e.target.value);
+    }
+
+    function emailInputFocus() {
+        emailLabelRef.current.className = css.labelTopFocus;
+    }
+
+    function emailInputBlur() {
+        emailLabelRef.current.className = email ? css.labelTopBlur : css.labelMiddle;
+    }
+
+    function pwdHandler(e) {
+        setPwd(e.target.value);
+    }
+
+    function pwdInputFocus() {
+        pwdLabelRef.current.className = css.labelTopFocus;
+    }
+
+    function pwdInputBlur() {
+        pwdLabelRef.current.className = pwd ? css.labelTopBlur : css.labelMiddle;
+    }
+
+    const css = {
+        labelMiddle: 'absolute left-3 bottom-1/2 translate-y-1/2 text-base text-purple-text transition-transform cursor-text',
+        labelTopBlur: 'px-2 absolute left-1 -top-3 text-sm text-purple-text bg-purple-background transition-transform cursor-text',
+        labelTopFocus: 'px-2 absolute left-1 -top-3 text-sm text-purple-primary bg-purple-background transition-transform cursor-text'
+    }
 
     return (
-        <div className="flex absolute right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2 overflow-hidden rounded-2xl tablet:rounded-3xl" style={{boxShadow: `0 0 4px ${themeConfig.purple.shadow}`}}>
+        <div className="flex absolute right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2 overflow-hidden shadow-sm shadow-purple-shadow rounded-2xl tablet:rounded-3xl">
             {
                 tabletBreakpoint &&
                 <section className="w-56 h-auto phone:w-60 tablet:w-80 desktop:w-96 bg-cover bg-center bg-no-repeat" style={{backgroundImage: 'url("/Login Image.jpeg")'}} />
             }
-            <section className="w-56 phone:w-60 tablet:w-80 desktop:w-96 p-8 flex flex-col gap-10">
+            <section className="w-56 phone:w-64 tablet:w-80 desktop:w-96 p-8 flex flex-col gap-12">
                 <img src="/Logo.png" alt="R-Tracker Logo" width="32px" height="32px" />
-                <form className="flex flex-col gap-4">
-                    <h1 className="text-xl" style={{color: themeConfig.purple.text}}>Login</h1>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" className={`w-full p-2 text-base border ${border} rounded-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`} />
-                    </div>
-                    <button style={{color: themeConfig.purple.oppositeText, backgroundColor: themeConfig.purple.primary}}>Login</button>
+                <form className="flex flex-col gap-6">
+                    <h1 className="text-2xl text-purple-text">Login</h1>
+                    <section className="flex flex-col gap-4">
+                        <div className="relative">
+                            <label htmlFor="email" ref={emailLabelRef} className={css.labelMiddle}>Email</label>
+                            <input type="email" id="email" value={email} onChange={emailHandler} onFocus={emailInputFocus} onBlur={emailInputBlur} className="w-full px-3 py-2 text-base text-purple-text border border-purple-neutral rounded-md outline-none focus:border-purple-primary" />
+                        </div>
+                        <div className="relative">
+                            <label htmlFor="pwd" ref={pwdLabelRef} className={css.labelMiddle}>Password</label>
+                            <input type="password" id="pwd" value={pwd} onChange={pwdHandler} onFocus={pwdInputFocus} onBlur={pwdInputBlur} className="w-full px-3 py-2 text-base text-purple-text border border-purple-neutral rounded-md outline-none focus:border-purple-primary" />
+                        </div>
+                        <button className="py-2 text-base text-purple-oppositeText bg-purple-primary rounded-md hover:bg-purple-highlight">Login</button>
+                    </section>
                 </form>
                 <footer className="flex flex-col items-center gap-2">
-                    <small className="text-sm" style={{color: themeConfig.purple.text}}>Contact us</small>
-                    <span className="flex gap-2 tablet:gap-4">
+                    <small className="text-sm text-purple-text">Contact us</small>
+                    <span className="flex gap-4 tablet:gap-6">
                         <a href={process.env.GITHUB_URI} target="_blank" rel="noopener noreferrer" >
-                            <img src="/Github.png" alt="Github Icon" width="24px" height="24px" />
+                            <img src="/Github.png" alt="Github Icon" width="32px" height="32px" />
                         </a>
                         <a href={process.env.LINKEDIN_URI} target="_blank" rel="noopener noreferrer" >
-                            <img src="/Linkedin.png" alt="Linkedin Icon" width="24px" height="24px" />
+                            <img src="/Linkedin.png" alt="Linkedin Icon" width="32px" height="32px" />
                         </a>
                         <a href={process.env.INSTAGRAM_URI} target="_blank" rel="noopener noreferrer" >
-                            <img src="/Instagram.png" alt="Instagram Icon" width="24px" height="24px" />
+                            <img src="/Instagram.png" alt="Instagram Icon" width="32px" height="32px" />
                         </a>
                         <a href={process.env.WHATSAPP_URI} target="_blank" rel="noopener noreferrer" >
-                            <img src="/Whatsapp.png" alt="Whatsapp Icon" width="24px" height="24px" />
+                            <img src="/Whatsapp.png" alt="Whatsapp Icon" width="32px" height="32px" />
                         </a>
                     </span>
                 </footer>
