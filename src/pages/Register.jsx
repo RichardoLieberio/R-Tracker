4dbd6350e4a6bb7832633ec3a00beae216e74ba4
Richardo Lieberio <richardo.alt@gmail.com>
2024-10-31 16:56:20 +0700

    Register form state moved to register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,38 @@
+import {useState} from 'react';
+
 import RegisterForm from '../components/RegisterForm';
+import EmailVerification from '../components/EmailVerification';
 
 export default function Register() {
-    return (
-        <RegisterForm />
-    );
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [confPwd, setConfPwd] = useState('');
+    const [showPwd, setShowPwd] = useState(false);
+    const [showConfPwd, setShowConfPwd] = useState(false);
+    const [step, setStep] = useState('register');
+
+    function stepHandler(nextStep) {
+        setStep(nextStep);
+    }
+
+    switch (step) {
+        case 'register':
+            const registerFormState = {
+                name, setName,
+                email, setEmail,
+                pwd, setPwd,
+                confPwd, setConfPwd,
+                showPwd, setShowPwd,
+                showConfPwd, setShowConfPwd,
+                stepHandler
+            };
+            return <RegisterForm {...registerFormState} />;
+        case 'verification':
+            const emailVerificationState = {
+                email,
+                stepHandler
+            };
+            return <EmailVerification {...emailVerificationState} />;
+    }
 }
