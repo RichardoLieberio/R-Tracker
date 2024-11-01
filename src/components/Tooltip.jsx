79062b5f72e133d4508d970734e6479f20c9cf16
Richardo Lieberio <richardo.alt@gmail.com>
2024-11-01 10:56:40 +0700

    Added posX and posY properties for offset position

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -2,9 +2,34 @@ import PropTypes from 'prop-types';
 
 import OriginalTooltip from '@mui/material/Tooltip';
 
-export default function Tooltip({title, children, className, ...props}) {
+export default function Tooltip({title, children, className, posX, posY, ...props}) {
+    const slotProps = {
+        tooltip: {
+            sx: {
+                width: 'auto',
+                margin: '0',
+                padding: '0',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                bgcolor: 'transparent',
+                pointerEvents: 'none'
+            }
+        },
+        popper: {
+            modifiers: [
+                {
+                    name: 'offset',
+                    options: {
+                        offset: [posX, posY]
+                    }
+                }
+            ]
+        }
+    };
+
     return (
-        <OriginalTooltip enterTouchDelay={0} {...props} title={<span className={className}>{title}</span>} slotProps={{tooltip: {sx: {width: 'auto', margin: '0', padding: '0', display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none'}}}}>
+        <OriginalTooltip enterTouchDelay={0} {...props} title={<span className={className}>{title}</span>} slotProps={slotProps}>
             <span>{children}</span>
         </OriginalTooltip>
     );
@@ -14,5 +39,7 @@ export default function Tooltip({title, children, className, ...props}) {
 Tooltip.propTypes = {
     title: PropTypes.string,
     children: PropTypes.node,
-    className: PropTypes.string
+    className: PropTypes.string,
+    posX: PropTypes.number,
+    posY: PropTypes.number
 };
