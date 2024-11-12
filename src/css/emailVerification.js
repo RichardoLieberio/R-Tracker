8ad8e9af99f0d103141232be4c92ea897cc456ca
Richardo Lieberio <richardo.alt@gmail.com>
2024-11-12 14:24:58 +0700

    Added css classes for emailVerification

---
+++ src/css/emailVerification.js
@@ -0,0 +1,4 @@
+export default {
+    otp: '!w-8 !h-10 tablet:!w-10 tablet:!h-12 text-2xl text-purple-text border border-purple-neutral outline-purple-primary rounded-md',
+    disabledOtp: '!w-8 !h-10 tablet:!w-10 tablet:!h-12 text-2xl text-purple-text bg-purple-disabled border border-purple-neutral rounded-md cursor-not-allowed'
+}
