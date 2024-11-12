ea7a8d9f7e86ed247c5dead5aad629970f79fb6c
Richardo Lieberio <richardo.alt@gmail.com>
2024-11-12 10:46:20 +0700

    Removed back button from verification

--- src/components/EmailVerification.jsx
+++ src/components/EmailVerification.jsx
@@ -1,25 +1,14 @@
 import PropTypes from 'prop-types';
 import {useState} from 'react';
 
-import {IoArrowBack} from 'react-icons/io5';
 import OtpInput from 'react-otp-input';
 
 export default function VerifyEmail(props) {
-    const {
-        email,
-        stepHandler
-    } = props;
+    const {email} = props;
     const [otp, setOtp] = useState('');
-
-    function back() {
-        stepHandler('register');
-    }
 
     return (
         <section className="w-1/3 min-w-56 phone:min-w-72 tablet:min-w-80 desktop:min-w-96 h-full mx-auto py-8 flex flex-col gap-12">
-            <section onClick={back} className="w-fit cursor-pointer">
-                <IoArrowBack className="text-2xl text-purple-text" />
-            </section>
             <section className="flex flex-col gap-8">
                 <img src="/Email Verification.png" alt="Email Verification" className="w-40 phone:w-48 tablet:w-56 desktop:w-64 mx-auto" />
                 <div className="flex flex-col gap-4">
@@ -46,6 +35,5 @@ export default function VerifyEmail(props) {
 }
 
 VerifyEmail.propTypes = {
-    email: PropTypes.string,
-    stepHandler: PropTypes.func
+    email: PropTypes.string
 };
