636a1e2e6f30c044bdcd9016c1f139b5f3f337db
Richardo Lieberio <richardo.alt@gmail.com>
2024-11-13 15:22:00 +0700

    Save user info after successfully verified

--- src/controllers/emailVerification.js
+++ src/controllers/emailVerification.js
@@ -3,7 +3,7 @@ import {toast} from 'react-toastify';
 import axios from '../services/axios';
 import {setToast} from '../services/toastService';
 
-import {setAccessToken} from '../redux/authSlice';
+import {setAccessToken, setUserInfo} from '../redux/authSlice';
 
 async function resendOtp(name, email, pwd, confPwd, csrfToken, accessToken, setFormError, setStep) {
     const data = {name, email, pwd, confPwd};
@@ -54,6 +54,7 @@ async function verify(email, otp, csrfToken, accessToken, setFormError, setOtpEr
     switch (status) {
         case 201:
             dispatch(setAccessToken(response.data.accessToken));
+            dispatch(setUserInfo(response.data.user));
             setStep('verified');
             break;
         case 400:
