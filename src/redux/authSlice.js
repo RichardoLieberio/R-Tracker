fa1aa39ec2be8390ca6313dbbfca2fb8327c9f99
Richardo Lieberio <richardo.alt@gmail.com>
2024-11-13 15:21:00 +0700

    Added setUserInfo and clearUserInfo

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -12,10 +12,16 @@ export const authSlice = createSlice({
         },
         clearAccessToken: function(state) {
             state.accessToken = null;
+        },
+        setUserInfo: function(state, action) {
+            state.userInfo = action.payload;
+        },
+        clearUserInfo: function(state) {
+            state.userInfo = null;
         }
     }
 });
 
-export const {setAccessToken, clearAccessToken} = authSlice.actions;
+export const {setAccessToken, clearAccessToken, setUserInfo, clearUserInfo} = authSlice.actions;
 
 export default authSlice.reducer;
