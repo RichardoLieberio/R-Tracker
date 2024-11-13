bd033f629a6177b9474e9edbd52162ec12f48fde
Richardo Lieberio <richardo.alt@gmail.com>
2024-11-13 14:49:56 +0700

    Uses getBackgroundColor function from css

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import {useSelector} from 'react-redux';
 
+import {getBackgroundColor} from '../css/color';
+
 export default function MainLayout({children}) {
     const theme = useSelector((state) => state.theme.color);
 
     return (
-        <main className="w-full min-w-60 h-screen relative" style={{backgroundColor: theme.background}}>
+        <main className={`w-full min-w-60 h-screen relative ${getBackgroundColor(theme)}`}>
             {children}
         </main>
     );
