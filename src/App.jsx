d02bfb31e7baa8bd63af11ab21e2aa2c6ec55591
Richardo Lieberio <richardo.alt@gmail.com>
2024-11-13 14:41:23 +0700

    Uses get color function

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,22 @@ import User from './pages/User';
 import ExpenseCategory from './pages/ExpenseCategory';
 
 import 'react-toastify/dist/ReactToastify.css';
+import getToastClassName from './css/toast';
+import {getTextColor} from './css/color';
 
-import MainLayout from './components/MainLayout';
 import {ToastContainer} from 'react-toastify';
-
-import {getToastClassName, getBodyClassName} from './css/toast';
+import MainLayout from './components/MainLayout';
 
 export default function App() {
     const theme = useSelector((state) => state.theme.color);
 
     function toastClassName(context) {
         const className = getToastClassName(theme);
-        return `${context.defaultClassName} ${className[context?.type]}`;
+        return `${context.defaultClassName} !${className[context?.type]}`;
     }
 
     function bodyClassName() {
-        const className = getBodyClassName(theme);
+        const className = getTextColor(theme);
         return `${className} ml-2 flex items-center text-base`;
     }
 
