faf5cc395789cb7f438cf86bc75da3c9419fe73c
Richardo Lieberio <richardo.alt@gmail.com>
2024-11-18 16:54:12 +0700

    Created css file for loginForm

---
+++ src/css/loginForm.js
@@ -0,0 +1,5 @@
+export default {
+    labelMiddle: 'absolute left-3 bottom-1/2 translate-y-1/2 text-base text-purple-text transition-transform cursor-text',
+    labelTopBlur: 'px-2 absolute left-1 -top-3 text-sm text-purple-text bg-purple-background transition-transform cursor-text',
+    labelTopFocus: 'px-2 absolute left-1 -top-3 text-sm text-purple-primary bg-purple-background transition-transform cursor-text'
+};
