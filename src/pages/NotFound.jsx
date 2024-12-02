5aeee8b324d88a375b14141eb88c2f101c1f78ba
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-02 12:06:21 +0700

    Changes text size

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,7 +8,7 @@ export default function NotFound() {
     return (
         <main className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center ${getTextColor(theme)}`}>
             <h1 className="text-6xl font-semibold">404</h1>
-            <strong className="text-xl">Page Not Found</strong>
+            <strong className="text-lg">Page Not Found</strong>
         </main>
     );
 }
