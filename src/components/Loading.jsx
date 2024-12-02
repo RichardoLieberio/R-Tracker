6dac7ff1d4d8d895c4524f986d624b701868ab31
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-02 11:41:48 +0700

    Modified loading component to uses color from theme

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
 
+import themeConfig from '../../config/theme';
+
 import {HashLoader} from 'react-spinners';
 
 export default function Loading(props) {
-    const {size, color} = props;
+    const {size, theme} = props;
 
     return (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <HashLoader size={size} color={color} />
+            <HashLoader size={size} color={themeConfig[theme].primary} />
         </div>
     );
 }
 
 Loading.propTypes = {
     size: PropTypes.number,
-    color: PropTypes.string
+    theme: PropTypes.string
 };
