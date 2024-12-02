0230376249ca4a99a1918b5ddbae9e565ff949ef
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-02 13:21:31 +0700

    Controller for loginForm done

--- src/controllers/loginForm.js
+++ src/controllers/loginForm.js
@@ -1,3 +1,10 @@
+import {toast} from 'react-toastify';
+
+import axios from '../services/axios';
+import {setToast} from '../services/toastService';
+
+import {setAccessToken, setUserInfo} from '../redux/authSlice';
+
 import css from '../css/registerForm';
 
 function inputErrorHandler(error, value, label, input, pwd=false) {
@@ -10,4 +17,37 @@ function inputErrorHandler(error, value, label, input, pwd=false) {
     }
 }
 
-export default {inputErrorHandler};
+async function login(email, pwd, rememberMe, csrfToken, accessToken, setFormError, dispatch, navigate) {
+    const data = {email, pwd, rememberMe};
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+            'CSRF-Token': csrfToken
+        },
+        useAbortController: true
+    };
+
+    const response = await axios.post('/api/auth/login', data, config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 200:
+            dispatch(setAccessToken(response.data.accessToken));
+            dispatch(setUserInfo(response.data.userInfo));
+            navigate('/', {replace: true});
+            break;
+        case 401:
+            toast.error(response.data.msg);
+            break;
+        case 403:
+            setToast('error', response.data.msg);
+            location.reload();
+            break;
+        case 422:
+            setFormError(response.data.msg);
+            break;
+    }
+}
+
+export default {inputErrorHandler, login};
