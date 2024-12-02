b05d926d1aa580aeba326ed42b639c818ae4ae6f
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-02 14:39:59 +0700

    Created controller for ForgotPwdEmail

---
+++ src/controllers/forgotPwdEmail.js
@@ -0,0 +1,49 @@
+import {toast} from 'react-toastify';
+
+import axios from '../services/axios';
+import {setToast} from '../services/toastService';
+
+import css from '../css/registerForm';
+
+function inputErrorHandler(error, value, label, input, pwd=false) {
+    if (error) {
+        label.current.className = value ? css.labelTopError : css.labelMiddleError;
+        input.current.className = pwd ? css.pwdInputError : css.defaultInputError;
+    } else {
+        label.current.className = value ? css.labelTopBlur : css.labelMiddle;
+        input.current.className = pwd ? css.pwdInput : css.defaultInput;
+    }
+}
+
+async function requestResetPwd(email, csrfToken, accessToken, setFormError, setStep) {
+    const data = {email};
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+            'CSRF-Token': csrfToken
+        },
+        useAbortController: true
+    };
+
+    const response = await axios.post('/api/request/reset-password', data, config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 200:
+            setStep('form');
+            break;
+        case 401:
+            toast.error(response.data.msg);
+            break;
+        case 403:
+            setToast('error', response.data.msg);
+            location.reload();
+            break;
+        case 422:
+            setFormError(response.data.msg);
+            break;
+    }
+}
+
+export default {inputErrorHandler, requestResetPwd};
