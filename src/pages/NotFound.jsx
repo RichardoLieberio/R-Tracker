14e63b168f75d7c343b3876ed7c3301f74ba37f0
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-02 12:03:58 +0700

    Created not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import {useSelector} from 'react-redux';
+
+import {getTextColor} from '../css/color';
+
+export default function NotFound() {
+    const theme = useSelector((state) => state.theme.color);
+
+    return (
+        <main className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center ${getTextColor(theme)}`}>
+            <h1 className="text-6xl font-semibold">404</h1>
+            <strong className="text-xl">Page Not Found</strong>
+        </main>
+    );
+}
