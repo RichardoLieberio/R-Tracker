17405c4ab0e9a97c54247a9ae8c0ce7a39b939e7
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-02 16:31:39 +0700

    Added resendOtp function

--- src/controllers/forgotPwdForm.js
+++ src/controllers/forgotPwdForm.js
@@ -15,6 +15,37 @@ function inputErrorHandler(error, value, label, input, pwd=false) {
     }
 }
 
+async function resendOtp(email, csrfToken, accessToken, setFormError) {
+    const data = {email};
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+            'CSRF-Token': csrfToken
+        },
+        useAbortController: true
+    };
+
+    const response = await axios.post('/api/request/reset-password', data, config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 200:
+            toast.success(response.data.msg);
+            break;
+        case 401:
+            toast.error(response.data.msg);
+            break;
+        case 403:
+            setToast('error', response.data.msg);
+            location.reload();
+            break;
+        case 422:
+            setFormError(response.data.msg);
+            break;
+    }
+}
+
 async function resetPwd(email, otp, pwd, confPwd, csrfToken, accessToken, setFormError, navigate) {
     const data = {email, otp, pwd, confPwd};
     const config = {
@@ -54,4 +85,4 @@ async function resetPwd(email, otp, pwd, confPwd, csrfToken, accessToken, setFor
     }
 }
 
-export default {inputErrorHandler, resetPwd};
+export default {inputErrorHandler, resendOtp, resetPwd};
