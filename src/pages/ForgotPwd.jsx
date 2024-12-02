9a457ef0847870d3e1cb6572f41de31cdc0dff3c
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-02 14:56:31 +0700

    Created page for password reset

--- src/pages/ForgotPwd.jsx
+++ src/pages/ForgotPwd.jsx
@@ -1,5 +1,29 @@
+import {useState, useEffect} from 'react';
+
+import {axiosController} from '../services/axios';
+
+import ForgotPwdEmail from '../components/ForgotPwdEmail';
+import ForgotPwdForm from '../components/ForgotPwdForm';
+
 export default function ForgotPwd() {
-    return (
-        <h1>Forgot Password Page</h1>
-    );
+    const [email, setEmail] = useState('');
+    const [step, setStep] = useState('form');
+
+    useEffect(function() {
+        return function() {
+            axiosController && axiosController.abort();
+        }
+    }, []);
+
+    switch (step) {
+        case 'email':
+            const forgotPwdEmailState = {
+                email, setEmail,
+                setStep
+            };
+            return <ForgotPwdEmail {...forgotPwdEmailState} />;
+        case 'form':
+            const forgotPwdFormState = {email};
+            return <ForgotPwdForm {...forgotPwdFormState} />;
+    }
 }
