d6fcca40dc541e297850372e6e125370d054476a
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-02 10:11:08 +0700

    Added abort controller is user navigates to another page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,16 @@
+import {useEffect} from 'react';
+
+import {axiosController} from '../services/axios';
+
 import LoginForm from '../components/LoginForm';
 
 export default function Login() {
+    useEffect(function() {
+        return function() {
+            axiosController && axiosController.abort();
+        }
+    }, []);
+
     return (
         <LoginForm />
     );
