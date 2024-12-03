88d46af43f2995c06fc072af20ef9d48d27385ba
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-03 13:43:34 +0700

    Added default size for loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,20 +1,34 @@
 import PropTypes from 'prop-types';
+import {useMediaQuery} from '@mui/material';
 
 import themeConfig from '../../config/theme';
+import breakpoints from '../../config/breakpoints';
 
 import {HashLoader} from 'react-spinners';
 
 export default function Loading(props) {
-    const {size, theme} = props;
+    const {defaultSize, size, theme} = props;
+
+    const phoneBreakpoint = useMediaQuery(`(min-width: ${breakpoints.phone})`);
+    const tabletBreakpoint = useMediaQuery(`(min-width: ${breakpoints.tablet})`);
+    const desktopBreakpoint = useMediaQuery(`(min-width: ${breakpoints.desktop})`);
+
+    const loadSize = defaultSize
+    ? desktopBreakpoint ? 80
+    : tabletBreakpoint ? 70
+    : phoneBreakpoint ? 60
+    : 50
+    : size;
 
     return (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <HashLoader size={size} color={themeConfig[theme].primary} />
+            <HashLoader size={loadSize} color={themeConfig[theme].primary} />
         </div>
     );
 }
 
 Loading.propTypes = {
+    defaultSize: PropTypes.bool,
     size: PropTypes.number,
     theme: PropTypes.string
 };
