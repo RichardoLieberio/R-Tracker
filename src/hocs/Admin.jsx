2c8db7054604d14f2e7696bc29ec3448826526ba
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-03 14:35:51 +0700

    Uses hocs controllers

--- src/hocs/Admin.jsx
+++ src/hocs/Admin.jsx
@@ -1,11 +1,30 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
+import {Navigate} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+
+import contr from '../controllers/hocs';
+
+import Loading from '../components/Loading';
 
 export default function Admin(Component) {
     return function AdminWrapper(props) {
-        useEffect(function() {
-            console.log('Admin');
-        }, []);
+        const [loading, setLoading] = useState(true);
 
-        return <Component {...props} />;
+        const theme = useSelector((state) => state.theme.color);
+        const accessToken = useSelector((state) => state.auth.accessToken);
+        const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+        const isAdmin = useSelector((state) => state.auth.isAdmin);
+
+        useEffect(function() {
+            if (!isAuthenticated) contr.getInfo(setLoading, accessToken);
+        }, [loading]); // eslint-disable-line react-hooks/exhaustive-deps
+
+        return isAdmin
+        ? <Component {...props} />
+        : isAuthenticated
+            ? <Navigate to="/" />
+            : loading
+                ? <Loading defaultSize theme={theme} />
+                : <Navigate to="/login" />;
     }
 }
--- src/hocs/Authenticated.jsx
+++ src/hocs/Authenticated.jsx
@@ -1,11 +1,27 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
+import {Navigate} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+
+import contr from '../controllers/hocs';
+
+import Loading from '../components/Loading';
 
 export default function Authenticated(Component) {
     return function AuthenticatedWrapper(props) {
-        useEffect(function() {
-            console.log('Authenticated');
-        }, []);
+        const [loading, setLoading] = useState(true);
 
-        return <Component {...props} />;
+        const theme = useSelector((state) => state.theme.color);
+        const accessToken = useSelector((state) => state.auth.accessToken);
+        const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+        useEffect(function() {
+            if (!isAuthenticated) contr.getInfo(setLoading, accessToken);
+        }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+        return isAuthenticated
+        ? <Component {...props} />
+        : loading
+            ? <Loading defaultSize theme={theme} />
+            : <Navigate to="/login" />;
     }
 }
