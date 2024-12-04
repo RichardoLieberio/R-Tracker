351602142084b17215f09b22958ab0f38199ac55
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-04 11:32:22 +0700

    All classes are important

--- src/css/color.js
+++ src/css/color.js
@@ -1,77 +1,115 @@
 function getBgPrimaryColor(theme) {
     switch (theme) {
         case 'red':
-            return 'bg-red-primary';
+            return '!bg-red-primary';
         case 'green':
-            return 'bg-green-primary';
+            return '!bg-green-primary';
         case 'blue':
-            return 'bg-blue-primary';
+            return '!bg-blue-primary';
         case 'yellow':
-            return 'bg-yellow-primary';
+            return '!bg-yellow-primary';
         case 'teal':
-            return 'bg-teal-primary';
+            return '!bg-teal-primary';
         case 'dark':
-            return 'bg-dark-primary';
+            return '!bg-dark-primary';
         default:
-            return 'bg-purple-primary';
+            return '!bg-purple-primary';
     }
 }
 
 function getBackgroundColor(theme) {
     switch (theme) {
         case 'red':
-            return 'bg-red-background';
+            return '!bg-red-background';
         case 'green':
-            return 'bg-green-background';
+            return '!bg-green-background';
         case 'blue':
-            return 'bg-blue-background';
+            return '!bg-blue-background';
         case 'yellow':
-            return 'bg-yellow-background';
+            return '!bg-yellow-background';
         case 'teal':
-            return 'bg-teal-background';
+            return '!bg-teal-background';
         case 'dark':
-            return 'bg-dark-background';
+            return '!bg-dark-background';
         default:
-            return 'bg-purple-background';
+            return '!bg-purple-background';
+    }
+}
+
+function getHoverBgHighlightColor(theme) {
+    switch (theme) {
+        case 'red':
+            return 'hover:!bg-red-highlight';
+        case 'green':
+            return 'hover:!bg-green-highlight';
+        case 'blue':
+            return 'hover:!bg-blue-highlight';
+        case 'yellow':
+            return 'hover:!bg-yellow-highlight';
+        case 'teal':
+            return 'hover:!bg-teal-highlight';
+        case 'dark':
+            return 'hover:!bg-dark-highlight';
+        default:
+            return 'hover:!bg-purple-highlight';
     }
 }
 
 function getTextColor(theme) {
     switch (theme) {
         case 'red':
-            return 'text-red-text';
+            return '!text-red-text';
         case 'green':
-            return 'text-green-text';
+            return '!text-green-text';
         case 'blue':
-            return 'text-blue-text';
+            return '!text-blue-text';
         case 'yellow':
-            return 'text-yellow-text';
+            return '!text-yellow-text';
         case 'teal':
-            return 'text-teal-text';
+            return '!text-teal-text';
         case 'dark':
-            return 'text-dark-text';
+            return '!text-dark-text';
         default:
-            return 'text-purple-text';
+            return '!text-purple-text';
     }
 }
 
 function getOppositeTextColor(theme) {
     switch (theme) {
         case 'red':
-            return 'text-red-oppositeText';
+            return '!text-red-oppositeText';
         case 'green':
-            return 'text-green-oppositeText';
+            return '!text-green-oppositeText';
         case 'blue':
-            return 'text-blue-oppositeText';
+            return '!text-blue-oppositeText';
         case 'yellow':
-            return 'text-yellow-oppositeText';
+            return '!text-yellow-oppositeText';
         case 'teal':
-            return 'text-teal-oppositeText';
+            return '!text-teal-oppositeText';
         case 'dark':
-            return 'text-dark-oppositeText';
+            return '!text-dark-oppositeText';
         default:
-            return 'text-purple-oppositeText';
+            return '!text-purple-oppositeText';
     }
 }
 
-export {getBgPrimaryColor, getBackgroundColor, getTextColor, getOppositeTextColor};
+function getTextHighlightColor(theme) {
+    switch (theme) {
+        case 'red':
+            return '!text-red-highlight';
+        case 'green':
+            return '!text-green-highlight';
+        case 'blue':
+            return '!text-blue-highlight';
+        case 'yellow':
+            return '!text-yellow-highlight';
+        case 'teal':
+            return '!text-teal-highlight';
+        case 'dark':
+            return '!text-dark-highlight';
+        default:
+            return '!text-purple-highlight';
+    }
+}
+
+export {getBgPrimaryColor, getBackgroundColor, getHoverBgHighlightColor, getTextColor, getOppositeTextColor, getTextHighlightColor};
