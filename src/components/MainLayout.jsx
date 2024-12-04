0ec8af4b1242239894f954c76a8dfe86d5dccd2d
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-04 11:33:09 +0700

    Fixed main layout height bug

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -7,7 +7,7 @@ export default function MainLayout({children}) {
     const theme = useSelector((state) => state.theme.color);
 
     return (
-        <main className={`w-full min-w-60 h-screen relative ${getBackgroundColor(theme)}`}>
+        <main className={`w-full min-w-60 min-h-screen relative ${getBackgroundColor(theme)}`}>
             {children}
         </main>
     );
