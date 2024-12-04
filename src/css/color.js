304ae564a44f6711eedd4f8377f6c5cdfd7b5ea3
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-04 13:13:30 +0700

    Added more functions

--- src/css/color.js
+++ src/css/color.js
@@ -112,4 +112,67 @@ function getTextHighlightColor(theme) {
     }
 }
 
-export {getBgPrimaryColor, getBackgroundColor, getHoverBgHighlightColor, getTextColor, getOppositeTextColor, getTextHighlightColor};
+function getHoverTextHighlightColor(theme) {
+    switch (theme) {
+        case 'red':
+            return 'hover:!text-red-highlight';
+        case 'green':
+            return 'hover:!text-green-highlight';
+        case 'blue':
+            return 'hover:!text-blue-highlight';
+        case 'yellow':
+            return 'hover:!text-yellow-highlight';
+        case 'teal':
+            return 'hover:!text-teal-highlight';
+        case 'dark':
+            return 'hover:!text-dark-highlight';
+        default:
+            return 'hover:!text-purple-highlight';
+    }
+}
+
+function getFromSecondaryColor(theme) {
+    switch (theme) {
+        case 'red':
+            return '!from-red-secondary';
+        case 'green':
+            return '!from-green-secondary';
+        case 'blue':
+            return '!from-blue-secondary';
+        case 'yellow':
+            return '!from-yellow-secondary';
+        case 'teal':
+            return '!from-teal-secondary';
+        case 'dark':
+            return '!from-dark-secondary';
+        default:
+            return '!from-purple-secondary';
+    }
+}
+
+function getToPrimaryColor(theme) {
+    switch (theme) {
+        case 'red':
+            return '!to-red-primary';
+        case 'green':
+            return '!to-green-primary';
+        case 'blue':
+            return '!to-blue-primary';
+        case 'yellow':
+            return '!to-yellow-primary';
+        case 'teal':
+            return '!to-teal-primary';
+        case 'dark':
+            return '!to-dark-primary';
+        default:
+            return '!to-purple-primary';
+    }
+}
+
+export {
+    getBgPrimaryColor, getBackgroundColor,
+    getHoverBgHighlightColor,
+    getTextColor, getOppositeTextColor, getTextHighlightColor,
+    getHoverTextHighlightColor,
+    getFromSecondaryColor, getToPrimaryColor
+};
