49665877fbc00449edb6546ef0e1a01035b2b233
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-04 13:39:45 +0700

    Header linked by useEffect

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,4 +1,23 @@
+import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+
+import {axiosController} from '../services/axios';
+
+import {changePage} from '../redux/webSlice';
+
 export default function Chart() {
+    const location = useLocation();
+    const dispatch = useDispatch();
+
+    useEffect(function() {
+        dispatch(changePage(location.pathname));
+
+        return function() {
+            axiosController && axiosController.abort();
+        }
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
     return (
         <h1>Chart Page</h1>
     );
--- src/pages/Expense.jsx
+++ src/pages/Expense.jsx
@@ -1,19 +1,28 @@
 import {useEffect} from 'react';
-import {useSelector} from 'react-redux';
+import {useLocation} from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
 
 import {axiosController} from '../services/axios';
+
+import {changePage} from '../redux/webSlice';
 
 import {HelmetProvider} from 'react-helmet-async';
 import ExpenseHead from '../head/ExpenseHead';
 
 export default function Expense() {
+    const location = useLocation();
+
     const accessToken = useSelector((state) => state.auth.accessToken);
 
+    const dispatch = useDispatch();
+
     useEffect(function() {
+        dispatch(changePage(location.pathname));
+
         return function() {
             axiosController && axiosController.abort();
         }
-    }, []);
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <HelmetProvider>
