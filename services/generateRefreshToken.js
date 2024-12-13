7bfb7eaa250e91f180c338d4191aed2ade4ae2aa
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-13 10:43:58 +0700

    Removed secure true options for cookies

--- services/generateRefreshToken.js
+++ services/generateRefreshToken.js
@@ -4,7 +4,6 @@ function generateRefreshToken(res, user, rememberMe) {
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: rememberMe ? '30d' : '1h'});
     res.cookie(process.env.REFRESH_TOKEN_COOKIE, refreshToken, {
         httpOnly: true,
-        secure: true,
         maxAge: rememberMe ? 30 * 24 * 60 * 60 * 1000 : 60 * 60 * 1000,
         sameSite: 'strict'
     });
