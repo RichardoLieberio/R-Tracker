aebbb2dd3d3c00f57cda0185cac0334a078debb4
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-13 16:46:26 +0700

    Fixed background color bug in public routes

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types';
+import {useLocation} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 
 import {getBackgroundColor} from '../css/color';
 
 export default function MainLayout({children}) {
+    const location = useLocation();
+
     const theme = useSelector((state) => state.web.theme);
 
+    const noThemeRoutes = ['/login', '/register', '/forgot-password'];
+
     return (
-        <main className={`w-full min-w-60 min-h-screen relative ${getBackgroundColor(theme)}`}>
+        <main className={`w-full min-w-60 min-h-screen relative ${noThemeRoutes.includes(location.pathname) ? 'bg-purple-background' : getBackgroundColor(theme)}`}>
             {children}
         </main>
     );
