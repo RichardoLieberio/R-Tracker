0b9904e639830721274b5ab85b91a913e789fdb9
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-13 11:13:51 +0700

    Fixed hover issues on phone

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -1,11 +1,13 @@
+import {useState} from 'react';
 import {useSelector} from 'react-redux';
 
-import {getBgPrimaryColor, getHoverBgHighlightColor, getOppositeTextColor} from '../css/color';
+import {getBgPrimaryColor, getBgHighlightColor, getOppositeTextColor} from '../css/color';
 
 import {useScrollTrigger, Fade, Box, Fab} from '@mui/material';
 import {IoIosArrowUp} from 'react-icons/io';
 
 export default function ScrollTop() {
+    const [isHovered, setIsHovered] = useState(false);
     const theme = useSelector((state) => state.web.theme);
 
     function handleClick(e) {
@@ -16,7 +18,7 @@ export default function ScrollTop() {
     return (
         <Fade in={useScrollTrigger({disableHysteresis: true, threshold: 100})}>
             <Box onClick={handleClick} role="presentation" className="fixed bottom-4 right-4">
-                <Fab size="small" className={`${getBgPrimaryColor(theme)} ${getHoverBgHighlightColor(theme)} !shadow-none`}>
+                <Fab size="small" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} onClick={() => setIsHovered(false)} className={`${isHovered ? getBgHighlightColor(theme) : getBgPrimaryColor(theme)} !shadow-none`}>
                     <IoIosArrowUp className={`${getOppositeTextColor(theme)}`} />
                 </Fab>
             </Box>
