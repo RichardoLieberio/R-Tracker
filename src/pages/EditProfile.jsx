d622613b69e58b9c27d544d4ac447c22555e69ba
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-16 09:14:46 +0700

    Created default behaviour for edit profile page

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,5 +1,31 @@
+import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+
+import {axiosController} from '../services/axios';
+
+import {changePage} from '../redux/webSlice';
+
+import {HelmetProvider} from 'react-helmet-async';
+import EditProfileHead from '../head/EditProfileHead';
+
 export default function EditProfile() {
+    const location = useLocation();
+
+    const dispatch = useDispatch();
+
+    useEffect(function() {
+        dispatch(changePage(location.pathname));
+
+        return function() {
+            axiosController && axiosController.abort();
+        }
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
     return (
-        <h1>Edit Profile</h1>
+        <HelmetProvider>
+            <EditProfileHead />
+            <h1 className="text-dark-text">Edit Profile Page</h1>
+        </HelmetProvider>
     );
 }
