5b352240c1e74cabf493916922a4fccbe71f76df
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-17 11:59:49 +0700

    Added margin auto for horizontal alignment

--- src/css/emailVerification.js
+++ src/css/emailVerification.js
@@ -1,5 +1,5 @@
 export default {
-    otp: '!w-8 !h-10 tablet:!w-10 tablet:!h-12 text-2xl text-purple-text border border-purple-neutral outline-purple-primary rounded-md',
-    disabledOtp: '!w-8 !h-10 tablet:!w-10 tablet:!h-12 text-2xl text-purple-text bg-purple-disabled border border-purple-neutral rounded-md cursor-not-allowed',
-    errorOtp: '!w-8 !h-10 tablet:!w-10 tablet:!h-12 text-2xl text-purple-text border border-purple-error outline-purple-error rounded-md'
+    otp: '!w-8 !h-10 tablet:!w-10 tablet:!h-12 mx-auto text-2xl text-purple-text border border-purple-neutral outline-purple-primary rounded-md',
+    disabledOtp: '!w-8 !h-10 tablet:!w-10 tablet:!h-12 mx-auto text-2xl text-purple-text bg-purple-disabled border border-purple-neutral rounded-md cursor-not-allowed',
+    errorOtp: '!w-8 !h-10 tablet:!w-10 tablet:!h-12 mx-auto text-2xl text-purple-text border border-purple-error outline-purple-error rounded-md'
 }
