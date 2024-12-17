1e147090c62fffe22a370aedbb0324ed3c26ef3d
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-17 11:59:29 +0700

    Added styles for otp

--- src/css/editProfile.js
+++ src/css/editProfile.js
@@ -1,4 +1,4 @@
-import {getBackgroundColor, getDisabledBgColor, getTextPrimaryColor, getTextHighlightColor, getTextErrorColor, getBorderNeutralColor, getFocusBorderPrimaryColor, getFocusBorderHighlightColor, getBorderErrorColor} from './color';
+import {getBackgroundColor, getDisabledBgColor, getTextPrimaryColor, getTextHighlightColor, getTextErrorColor, getBorderNeutralColor, getFocusBorderPrimaryColor, getFocusBorderHighlightColor, getBorderErrorColor, getOutlinePrimaryColor, getOutlineTextColor, getOutlineErrorColor} from './color';
 
 export default function(theme) {
     return {
@@ -10,6 +10,9 @@ export default function(theme) {
         defaultInput: `w-full px-3 py-2 ${getBackgroundColor(theme)} border ${getBorderNeutralColor(theme)} rounded-md outline-none ${theme === 'dark' ? getFocusBorderHighlightColor(theme) : getFocusBorderPrimaryColor(theme)} ${getDisabledBgColor(theme)} disabled:cursor-not-allowed`,
         defaultInputError: `w-full px-3 py-2 ${getBackgroundColor(theme)} pl-10 border ${getBorderErrorColor(theme)} rounded-md outline-none`,
         pwdInput: `w-full px-3 py-2 pr-10 ${getBackgroundColor(theme)} border ${getBorderNeutralColor(theme)} rounded-md outline-none ${theme === 'dark' ? getFocusBorderHighlightColor(theme) : getFocusBorderPrimaryColor(theme)} ${getDisabledBgColor(theme)} disabled:cursor-not-allowed`,
-        pwdInputError: `w-full px-10 py-2 ${getBackgroundColor(theme)} border ${getBorderErrorColor(theme)} rounded-md outline-none`
+        pwdInputError: `w-full px-10 py-2 ${getBackgroundColor(theme)} border ${getBorderErrorColor(theme)} rounded-md outline-none`,
+        otp: `!w-8 !h-10 tablet:!w-10 tablet:!h-12 text-2xl border ${getBorderNeutralColor(theme)} ${theme === 'dark' ? getOutlineTextColor(theme) : getOutlinePrimaryColor(theme)} rounded-md`,
+        disabledOtp: `!w-8 !h-10 tablet:!w-10 tablet:!h-12 text-2xl bg-purple-disabled border ${getBorderNeutralColor(theme)} rounded-md cursor-not-allowed`,
+        errorOtp: `!w-8 !h-10 tablet:!w-10 tablet:!h-12 text-2xl border ${getBorderErrorColor(theme)} ${getOutlineErrorColor(theme)} rounded-md`
     };
 };
