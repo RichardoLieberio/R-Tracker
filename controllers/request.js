c18705e0a95ce2080d128d6b41ec65794c54c65d
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-18 10:31:12 +0700

    Request for change email will uses notFoundHandler

--- controllers/request.js
+++ controllers/request.js
@@ -1,5 +1,6 @@
 const generateOtp = require('../services/generateOtp');
 const sendMail = require('../services/mailService');
+const notFoundHandler = require('../services/notFoundHandler');
 
 const User = require('../models/User');
 const PwdResetToken = require('../models/PwdResetToken');
@@ -19,6 +20,9 @@ async function resetPwd(req, res) {
 async function changeEmail(req, res) {
     const otp = generateOtp(+process.env.OTP_LENGTH);
     await ChangeEmailToken.addRequest(req.userId, req.data.email, otp);
+
+    const user = await User.getInfo(req.userId);
+    if (!user) return notFoundHandler(req, res, 'Failed to send otp. Account not found.')
 
     sendMail('new-email-verification', {to: req.data.email, otp});
 
