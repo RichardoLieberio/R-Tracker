ee1f440b568fda81804004b04b2d33a4ce29cd23
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-18 16:21:57 +0700

    Changed button spinner size to 6 for all devices

--- src/components/ButtonSpinner.jsx
+++ src/components/ButtonSpinner.jsx
@@ -1,19 +1,15 @@
 import PropTypes from 'prop-types';
 import {useSelector} from 'react-redux';
-import {useMediaQuery} from '@mui/material';
 
 import themes from '../../config/theme';
-import breakpoints from '../../config/breakpoints';
 
 import {BeatLoader} from 'react-spinners';
 
 export default function ButtonSpinner({noTheme, ...props}) {
     const theme = useSelector((state) => state.web.theme);
 
-    const tabletBreakpoint = useMediaQuery(`(min-width: ${breakpoints.tablet})`);
-
     return (
-        <BeatLoader color={themes[noTheme ? 'purple' : theme].oppositeText} size={tabletBreakpoint ? 8 : 6} className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" {...props} />
+        <BeatLoader color={themes[noTheme ? 'purple' : theme].oppositeText} size={6} className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" {...props} />
     );
 }
 
