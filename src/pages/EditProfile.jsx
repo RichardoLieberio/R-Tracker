85df3f03d5798e3837bf1c58b91e4cced4b57f41
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-18 11:18:04 +0700

    Change name and change email done

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -26,20 +26,31 @@ import EditProfileHead from '../head/EditProfileHead';
 import {MoonLoader} from 'react-spinners';
 import {FaPencilAlt, FaEye, FaEyeSlash} from 'react-icons/fa';
 import EditNameModal from '../components/EditNameModal';
+import EditEmailModal from '../components/EditEmailModal';
 
 export default function EditProfile() {
+    const [csrfToken, setCSRFToken] = useState('');
+
     const [newName, setNewName] = useState('');
     const [nameModal, setNameModal] = useState(false);
     const [savingNewName, setSavingNewName] = useState(false);
     const [nameError, setNameError] = useState({});
 
-    const [csrfToken, setCSRFToken] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+    const [emailModal, setEmailModal] = useState(false);
+    const [savingNewEmail, setSavingNewEmail] = useState(false);
+    const [emailError, setEmailError] = useState({});
+
+    const [otp, setOtp] = useState('');
+    const [otpError, setOtpError] = useState('');
+    const [step, setStep] = useState('email');
+
     const [pwd, setPwd] = useState('');
     const [showPwd, setShowPwd] = useState(false);
     const [confPwd, setConfPwd] = useState('');
     const [showConfPwd, setShowConfPwd] = useState(false);
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [formError, setFormError] = useState({});
+    const [savingNewPwd, setSavingNewPwd] = useState(false);
+    const [pwdError, setPwdError] = useState({});
 
     const pwdLabelRef = useRef(null);
     const pwdInputRef = useRef(null);
@@ -79,17 +90,56 @@ export default function EditProfile() {
         }
     }, [hasToggled, showConfPwd, confPwd]);
 
+    function openNameModal() {
+        !savingNewName && setNameModal(true);
+    }
+
+    async function saveName() {
+        if (!savingNewName) {
+            setSavingNewName(true);
+            setNameError({});
+            await contr.changeName(newName, csrfToken, accessToken, setNameError, setNewName, userInfo.name, setNameModal);
+            setSavingNewName(false);
+        }
+    }
+
+    function openEmailModal() {
+        !(savingNewEmail && step !== 'email') && setEmailModal(true);
+    }
+
+    async function requestChangeEmail() {
+        if (!savingNewEmail) {
+            setSavingNewEmail(true);
+            setEmailError({});
+            await contr.requestChangeEmail(newEmail, csrfToken, accessToken, setEmailError, userInfo.email, setStep);
+            setSavingNewEmail(false);
+        }
+    }
+
+    function resendOtp() {
+        contr.resendOtp(newEmail, csrfToken, accessToken, setEmailError, setStep);
+    }
+
+    async function changeEmail() {
+        if (!savingNewEmail) {
+            setSavingNewEmail(true);
+            setOtpError('');
+            await contr.changeEmail(newEmail, setNewEmail, setEmailError, otp, setOtp, setOtpError, csrfToken, accessToken, setStep, setEmailModal);
+            setSavingNewEmail(false);
+        }
+    }
+
     function pwdHandler(e) {
         setPwd(e.target.value);
     }
 
     function pwdInputFocus() {
-        if (formError.pwd) pwdLabelRef.current.className = css(theme).labelTopError
+        if (pwdError.pwd) pwdLabelRef.current.className = css(theme).labelTopError
         else pwdLabelRef.current.className = css(theme).labelTopFocus;
     }
 
     function pwdInputBlur() {
-        if (formError.pwd) pwdLabelRef.current.className = pwd ? css(theme).labelTopError : css(theme).labelMiddleError
+        if (pwdError.pwd) pwdLabelRef.current.className = pwd ? css(theme).labelTopError : css(theme).labelMiddleError
         else pwdLabelRef.current.className = pwd ? css(theme).labelTopBlur : css(theme).labelMiddle;
     }
 
@@ -98,12 +148,12 @@ export default function EditProfile() {
     }
 
     function confPwdInputFocus() {
-        if (formError.confPwd) confPwdLabelRef.current.className = css(theme).labelTopError
+        if (pwdError.confPwd) confPwdLabelRef.current.className = css(theme).labelTopError
         else confPwdLabelRef.current.className = css(theme).labelTopFocus;
     }
 
     function confPwdInputBlur() {
-        if (formError.confPwd) confPwdLabelRef.current.className = confPwd ? css(theme).labelTopError : css(theme).labelMiddleError
+        if (pwdError.confPwd) confPwdLabelRef.current.className = confPwd ? css(theme).labelTopError : css(theme).labelMiddleError
         else confPwdLabelRef.current.className = confPwd ? css(theme).labelTopBlur : css(theme).labelMiddle;
     }
 
@@ -117,20 +167,13 @@ export default function EditProfile() {
         hasToggled.current = true;
     }
 
-    async function saveName() {
-        if (!savingNewName) {
-            setNameModal(false);
-            setSavingNewName(true);
-            setNameError({});
-            await contr.changeName(newName, csrfToken, accessToken, setNameError, setNewName, userInfo.name);
-            setSavingNewName(false);
-        }
-    }
+    const editNameProps = {newName, setNewName, nameModal, setNameModal, nameError, savingNewName, saveName};
+    const editEmailProps = {step, setStep, newEmail, setNewEmail, emailError, otp, setOtp, otpError, emailModal, setEmailModal, savingNewEmail, requestChangeEmail, resendOtp, changeEmail};
 
     return (
         <HelmetProvider>
             <EditProfileHead />
-            <section className="w-56 phone:w-72 tablet:w-96 desktop:w-1/3 desktop:min-w-96 mx-auto py-12 flex flex-col gap-12">
+            <section className="w-1/3 min-w-56 phone:min-w-72 tablet:min-w-80 desktop:min-w-96 mx-auto py-12 flex flex-col gap-12">
                 <section className="flex flex-col gap-8">
                     <header className="flex flex-col gap-1">
                         <h2 className="text-xl font-semibold">My Account</h2>
@@ -140,15 +183,26 @@ export default function EditProfile() {
                         <div className="relative flex flex-col">
                             <span className={`px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)} transition-transform cursor-text`}>Name</span>
                             <span className={`w-full px-3 py-2 pr-10 truncate border ${getBorderNeutralColor(theme)} rounded-md`}>{userInfo.name}</span>
-                            <div onClick={() => !savingNewName && setNameModal(true)} className={`px-3 py-3 absolute right-0 top-0 rounded-tr-md rounded-br-md ${savingNewName ? 'cursor-default' : 'cursor-pointer'}`}>
-                                {savingNewName ? <MoonLoader size={16} color={themeConfig[theme].text} /> : <FaPencilAlt />}
+                            <div onClick={openNameModal} className={`px-3 py-3 absolute right-0 top-0 rounded-tr-md rounded-br-md ${savingNewName ? 'cursor-default' : 'cursor-pointer'}`}>
+                                {
+                                    savingNewName
+                                    ? <MoonLoader size={16} color={themeConfig[theme].text} />
+                                    : <FaPencilAlt />
+                                }
                             </div>
-                            <EditNameModal newName={newName} setNewName={setNewName} nameModal={nameModal} setNameModal={setNameModal} savingNewName={savingNewName} nameError={nameError} saveName={saveName} />
+                            <EditNameModal {...editNameProps} />
                         </div>
                         <div className="relative flex flex-col">
                             <span className={`px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)} transition-transform cursor-text`}>Email</span>
                             <span className={`w-full px-3 py-2 pr-10 truncate border ${getBorderNeutralColor(theme)} rounded-md`}>{userInfo.email}</span>
-                            <div className="px-3 py-3 absolute right-0 top-0 rounded-tr-md rounded-br-md cursor-pointer"><FaPencilAlt /></div>
+                            <div onClick={openEmailModal} className={`px-3 py-3 absolute right-0 top-0 rounded-tr-md rounded-br-md ${savingNewEmail ? 'cursor-default' : 'cursor-pointer'}`}>
+                                {
+                                    savingNewEmail && step !== 'email'
+                                    ? <MoonLoader size={16} color={themeConfig[theme].text} />
+                                    : <FaPencilAlt />
+                                }
+                            </div>
+                            <EditEmailModal {...editEmailProps} />
                         </div>
                     </main>
                 </section>
@@ -159,9 +213,9 @@ export default function EditProfile() {
                     </header>
                     <main>
                         <form action="" className="flex flex-col gap-4" autoCapitalize="off" autoComplete="off" spellCheck="false">
-                            <div className="relative flex flex-col">
+                            <div className="relative">
                                 <label htmlFor="pwd" ref={pwdLabelRef} className={css(theme).labelMiddle}>New Password</label>
-                                <input type={showPwd ? "text" : "password"} id="pwd" value={pwd} ref={pwdInputRef} disabled={isSubmitting} onChange={pwdHandler} onFocus={pwdInputFocus} onBlur={pwdInputBlur} className={css(theme).pwdInput} />
+                                <input type={showPwd ? "text" : "password"} id="pwd" value={pwd} ref={pwdInputRef} disabled={savingNewPwd} onChange={pwdHandler} onFocus={pwdInputFocus} onBlur={pwdInputBlur} className={css(theme).pwdInput} />
                                 <div onClick={togglePwd} className="px-3 py-3 absolute right-0 top-0 rounded-tr-md rounded-br-md cursor-pointer">
                                     {
                                         showPwd
@@ -170,9 +224,9 @@ export default function EditProfile() {
                                     }
                                 </div>
                             </div>
-                            <div className="relative flex flex-col">
+                            <div className="relative">
                                 <label htmlFor="confPwd" ref={confPwdLabelRef} className={css(theme).labelMiddle}>Confirm New Password</label>
-                                <input type={showConfPwd ? "text" : "password"} id="confPwd" value={confPwd} ref={confPwdInputRef} disabled={isSubmitting} onChange={confPwdHandler} onFocus={confPwdInputFocus} onBlur={confPwdInputBlur} className={css(theme).pwdInput} />
+                                <input type={showConfPwd ? "text" : "password"} id="confPwd" value={confPwd} ref={confPwdInputRef} disabled={savingNewPwd} onChange={confPwdHandler} onFocus={confPwdInputFocus} onBlur={confPwdInputBlur} className={css(theme).pwdInput} />
                                 <div onClick={toggleConfPwd} className="px-3 py-3 absolute right-0 top-0 rounded-tr-md rounded-br-md cursor-pointer">
                                     {
                                         showConfPwd
