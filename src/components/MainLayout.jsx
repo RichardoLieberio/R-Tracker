4cf2a7746c316d4e1c55ad3e5b41611c964d8fb2
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-19 11:29:49 +0700

    Always use theme color

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,22 +1,18 @@
 import PropTypes from 'prop-types';
-import {useLocation} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 
 import {getTextColor, getBackgroundColor} from '../css/color';
 
-export default function MainLayout({noThemeRoutes, children}) {
-    const location = useLocation();
-
+export default function MainLayout({children}) {
     const theme = useSelector((state) => state.web.theme);
 
     return (
-        <main className={`w-full min-w-60 min-h-screen relative text-base ${noThemeRoutes.includes(location.pathname) ? 'text-purple-text bg-purple-background' : `${getTextColor(theme)} ${getBackgroundColor(theme)}`}`}>
+        <main className={`w-full min-w-60 min-h-screen relative text-base ${getTextColor(theme)} ${getBackgroundColor(theme)}`}>
             {children}
         </main>
     );
 }
 
 MainLayout.propTypes = {
-    noThemeRoutes: PropTypes.array,
     children: PropTypes.node
 };
