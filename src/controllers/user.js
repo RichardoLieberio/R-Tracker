10da38e1076959561d24fb80b380158e6ada3e50
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-23 15:41:57 +0700

    Added controller to delete user account

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,9 @@
+import {toast} from 'react-toastify';
 import axios from '../services/axios';
 
 import store from '../redux/store';
-import {setUsers} from '../redux/dataSlice';
+import {setUsers, deleteUser} from '../redux/dataSlice';
+import {clearUser, addProcess, deleteProcess} from '../redux/userPageSlice';
 
 async function getAllUsers(accessToken) {
     const config = {
@@ -30,4 +32,45 @@ async function getAllUsers(accessToken) {
     }
 }
 
-export default {getAllUsers};
+async function deleteUserAccount(id, csrfToken, accessToken) {
+    store.dispatch(addProcess(id));
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+            'CSRF-Token': csrfToken
+        },
+        authenticated: {
+            codes: [401],
+            route: '/login'
+        },
+        adminRequest: {
+            codes: [403],
+            route: '/admin/user'
+        }
+    };
+
+    const response = await axios.delete(`/api/admin/user/${id}`, config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 200:
+            toast.success(response.data.msg);
+            store.dispatch(deleteUser(response.data.userId));
+            store.dispatch(clearUser(response.data.userId));
+            break;
+        case 400:
+            toast.error(response.data.msg);
+            break;
+        case 404:
+            toast.error(response.data.msg);
+            store.dispatch(deleteUser(response.data.userId));
+            store.dispatch(clearUser(response.data.userId));
+            break;
+    }
+
+    store.dispatch(deleteProcess(id));
+}
+
+export default {getAllUsers, deleteUserAccount};
