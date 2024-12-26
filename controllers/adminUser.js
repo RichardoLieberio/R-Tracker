d114b3d2dd904907a0a8c76e485e2f0589392008
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-26 10:08:34 +0700

    changePwd controller will return data to client

--- controllers/adminUser.js
+++ controllers/adminUser.js
@@ -39,12 +39,12 @@ async function changePwd(req, res) {
     if (!mongooseIdValidation(req.params.id)) throw new TransactionError({status: 404, msg: 'Failed to change user password. User not found.'});
     if (req.userId === req.params.id) throw new TransactionError({status: 400, msg: 'Cannnot change your own password.'});
 
-    const changed = await User.changePwdByAdmin(req.params.id, req.data.pwd, req.mongooseSession);
-    if (!changed) throw new TransactionError({status: 404, msg: 'Failed to change user password. User not found.'});
+    const data = await User.changePwdByAdmin(req.params.id, req.data.pwd, req.mongooseSession);
+    if (!data) throw new TransactionError({status: 404, msg: 'Failed to change user password. User not found.'});
 
     await UserToken.clearToken(req.params.id, req.mongooseSession);
 
-    res.json({status: 200, msg: 'User password changed successfully.'});
+    res.json({status: 200, msg: 'User password changed successfully.', data});
 }
 
 async function whitelist(req, res) {
