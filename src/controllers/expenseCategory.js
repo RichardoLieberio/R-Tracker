275a96aaf333d4b8680f5bd34804efb50e29773a
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-27 11:22:16 +0700

    Created controller to get and add category

--- src/controllers/expenseCategory.js
+++ src/controllers/expenseCategory.js
@@ -1,3 +1,9 @@
+import {toast} from 'react-toastify';
+import axios from '../services/axios';
+
+import store from '../redux/store';
+import {setExpenseCategories, addExpenseCategory} from '../redux/dataSlice';
+
 import css from '../css/expenseCategory';
 
 function inputErrorHandler(theme, error, value, setLabelClass, setInputClass, color=false) {
@@ -10,4 +16,63 @@ function inputErrorHandler(theme, error, value, setLabelClass, setInputClass, co
     }
 }
 
-export default {inputErrorHandler};
+async function getCategories(accessToken) {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+        authenticated: {
+            codes: [401],
+            route: '/login'
+        }
+    };
+
+    const response = await axios.get('/api/expense/categories', config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 200:
+            store.dispatch(setExpenseCategories(response.data.categories));
+            break;
+    }
+}
+
+async function addCategory(name, setName, color, setColor, icon, setIcon, csrfToken, accessToken, setModal, setError) {
+    const data = {name, color, icon};
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+            'CSRF-Token': csrfToken
+        },
+        authenticated: {
+            codes: [401],
+            route: '/login'
+        },
+        adminRequest: {
+            codes: [403],
+            route: '/admin/expense-category'
+        }
+    };
+
+    const response = await axios.post('/api/admin/expense-category', data, config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 201:
+            toast.success(response.data.msg);
+            store.dispatch(addExpenseCategory(response.data.data));
+            setModal(false);
+            setName('');
+            setColor('');
+            setIcon('');
+            break;
+        case 422:
+            setError(response.data.msg);
+            toast.error('Add category failed');
+            break;
+    }
+}
+
+export default {inputErrorHandler, getCategories, addCategory};
