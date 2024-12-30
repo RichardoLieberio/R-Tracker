a38d67fb8e65ac6f99a992eeb19a35b2a50a8fe1
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-30 09:30:52 +0700

    Expense category page used created card component to display expense category

--- src/pages/ExpenseCategory.jsx
+++ src/pages/ExpenseCategory.jsx
@@ -15,9 +15,9 @@ import {setOrder, setOrderBy, addProcess, deleteProcess} from '../redux/category
 import contr from '../controllers/expenseCategory';
 
 import {
-    getBgPrimaryColor, getBackgroundColor, getBgNeutral10Color,
+    getBgPrimaryColor, getBackgroundColor,
     getHoverBgNeutral50Color, getHoverBgHighlightColor,
-    getTextColor, getOppositeTextColor, getTextErrorColor,
+    getTextColor, getOppositeTextColor,
     getHoverTextHighlightColor,
     getBorderNeutralColor,
     getHoverBorderHighlightColor,
@@ -26,9 +26,9 @@ import {
 
 import {HelmetProvider} from 'react-helmet-async';
 import {Menu, MenuButton, MenuItems, MenuItem} from '@headlessui/react';
-import {BsThreeDotsVertical} from 'react-icons/bs';
-import {FaPencilAlt, FaEye, FaEyeSlash, FaTrashAlt, FaSortAlphaDown, FaSortAlphaUp} from 'react-icons/fa';
+import {FaSortAlphaDown, FaSortAlphaUp} from 'react-icons/fa';
 import ExpenseCategoryHead from '../head/ExpenseCategoryHead';
+import ExpenseCategoryCard from '../components/ExpenseCategoryCard';
 import AddCategoryModal from '../components/AddCategoryModal';
 import CategoryModal from '../components/CategoryModal';
 import ConfirmDeleteCategory from '../components/ConfirmDeleteCategory';
@@ -162,49 +162,7 @@ export default function ExpenseCategory() {
                 </header>
                 <main className={`grid ${fullLayout ? 'grid-cols-5' : 'phone:grid-cols-2 tablet:grid-cols-3 desktop:grid-cols-4'} gap-4 text-center`}>
                     {
-                        passCategory?.map(category => (
-                            <div onClick={() => openCategoryModal(category)} key={category._id} className={`w-full px-6 py-4 relative flex flex-col gap-2 ${category.hidden && getBgNeutral10Color(theme)} border ${getBorderNeutralColor(theme)} rounded-xl ${getHoverBgNeutral50Color(theme)} cursor-pointer`}>
-                                {category.hidden && <FaEyeSlash className="absolute top-4 left-4" />}
-                                <Menu>
-                                    <MenuButton onClick={e => e.stopPropagation()} className="p-1 absolute top-3 right-2">
-                                        <BsThreeDotsVertical />
-                                    </MenuButton>
-                                    <MenuItems transition anchor="bottom end" className={`w-36 mt-2 py-1 flex flex-col ${getTextColor(theme)} ${getBackgroundColor(theme)} shadow-lg ${getShadowColor(theme)} rounded-lg origin-top-right transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0`}>
-                                        <MenuItem>
-                                            <button className={`px-4 py-2 flex items-center gap-2 text-start ${getHoverBgNeutral50Color(theme)}`}>
-                                                <FaPencilAlt className="flex-shrink-0 text-lg" />
-                                                Edit
-                                            </button>
-                                        </MenuItem>
-                                        <MenuItem>
-                                            <button onClick={(e) => hideHandler(e, category)} className={`px-4 py-2 flex items-center gap-2 text-start ${getHoverBgNeutral50Color(theme)}`}>
-                                                {
-                                                    category.hidden
-                                                    ? <>
-                                                        <FaEye className="flex-shrink-0 text-lg" />
-                                                        Unhide
-                                                    </>
-                                                    : <>
-                                                        <FaEyeSlash className="flex-shrink-0 text-lg" />
-                                                        Hide
-                                                    </>
-                                                }
-                                            </button>
-                                        </MenuItem>
-                                        <MenuItem>
-                                            <button onClick={(e) => openDeleteModal(e, category)} className={`px-4 py-2 flex items-center gap-2 text-start ${getTextErrorColor(theme)} ${getHoverBgNeutral50Color(theme)}`}>
-                                                <FaTrashAlt className="flex-shrink-0 text-lg" />
-                                                Delete
-                                            </button>
-                                        </MenuItem>
-                                    </MenuItems>
-                                </Menu>
-                                <div className="p-3 mx-auto rounded-full" style={{backgroundColor: `#${category.color}`}}>
-                                    <img src={`${process.env.EXPENSE_CATEGORY_URI}/${category.icon}`} alt={category.name} className="w-10 h-10" />
-                                </div>
-                                <span className="truncate overflow-hidden">{category.name}</span>
-                            </div>
-                        ))
+                        passCategory?.map(category => <ExpenseCategoryCard key={category._id} category={category} openCategoryModal={openCategoryModal} openDeleteModal={openDeleteModal} hide={hideHandler} />)
                     }
                 </main>
                 <CategoryModal modal={categoryModal} setModal={setCategoryModal} category={category} />
