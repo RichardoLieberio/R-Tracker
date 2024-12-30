6ef82d15b1e5c1d977b805866ab41022b0deee97
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-30 09:34:34 +0700

    Fixed category modal to update when hide/unhide performed

--- src/pages/ExpenseCategory.jsx
+++ src/pages/ExpenseCategory.jsx
@@ -90,6 +90,7 @@ export default function ExpenseCategory() {
 
     useEffect(function() {
         if (expenseCategories) {
+            categoryModal && setCategory(expenseCategories.filter(cat => cat._id === category._id)[0]);
             deleteCategoryModal && !expenseCategories.some(cat => cat._id === category._id) && setDeleteCategoryModal(false);
         }
     }, [expenseCategories]); // eslint-disable-line react-hooks/exhaustive-deps
