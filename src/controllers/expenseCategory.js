84b7be6a56cb68d558157702909cebed6fbf8279
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-30 09:19:41 +0700

    Added controller to delete expense category

--- src/controllers/expenseCategory.js
+++ src/controllers/expenseCategory.js
@@ -139,4 +139,36 @@ async function unhideCategory(id, csrfToken, accessToken) {
     }
 }
 
-export default {inputErrorHandler, getCategories, addCategory, hideCategory, unhideCategory};
+async function deleteCategory(id, csrfToken, accessToken) {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+            'CSRF-Token': csrfToken
+        },
+        authenticated: {
+            codes: [401],
+            route: '/login'
+        },
+        adminRequest: {
+            codes: [403],
+            route: '/admin/expense-category'
+        }
+    };
+
+    const response = await axios.delete(`/api/admin/expense-category/${id}`, config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 200:
+            toast.success(response.data.msg);
+            store.dispatch(deleteExpenseCategory(response.data.id));
+            break;
+        case 404:
+            toast.error(response.data.msg);
+            store.dispatch(deleteExpenseCategory(id));
+            break;
+    }
+}
+
+export default {inputErrorHandler, getCategories, addCategory, hideCategory, unhideCategory, deleteCategory};
