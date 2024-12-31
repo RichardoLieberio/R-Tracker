47d9ffe316f468cf19372db2c3f5608115498deb
Richardo Lieberio <richardo.alt@gmail.com>
2024-12-31 16:55:19 +0700

    Modified expense page to have year and months button, use calendar component, and fetch expenses for first load

--- src/pages/Expense.jsx
+++ src/pages/Expense.jsx
@@ -1,23 +1,40 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+
+import months from '../../config/months';
 
 import {axiosController} from '../services/axios';
+import getCSRFToken from '../services/getCSRFToken';
 import {getToast} from '../services/toastService';
 
 import {changePage} from '../redux/webSlice';
 
+import contr from '../controllers/expense';
+
 import {HelmetProvider} from 'react-helmet-async';
+import {TiArrowSortedDown} from 'react-icons/ti';
 import ExpenseHead from '../head/ExpenseHead';
+import ExpenseCalendar from '../components/ExpenseCalendar';
 
 export default function Expense() {
+    const [csrfToken, setCSRFToken] = useState('');
+
     const location = useLocation();
+
+    const accessToken = useSelector((state) => state.auth.accessToken);
+    const expenses = useSelector((state) => state.data.expenses);
+    const year = useSelector((state) => state.expensePage.year);
+    const month = useSelector((state) => state.expensePage.month);
 
     const dispatch = useDispatch();
 
     useEffect(function() {
         getToast();
         dispatch(changePage(location.pathname));
+
+        getCSRFToken(setCSRFToken);
+        !expenses && contr.getExpenses(accessToken);
 
         return function() {
             axiosController && axiosController.abort();
@@ -27,7 +44,15 @@ export default function Expense() {
     return (
         <HelmetProvider>
             <ExpenseHead />
-            <h1>Expense Page</h1>
+            <section className="w-fit mx-auto py-8 pb-16 flex flex-col desktop:flex-auto gap-12 desktop:gap-24">
+                <section className="flex flex-col gap-8">
+                    <div className="flex items-center gap-8">
+                        <span className="flex items-center gap-2 cursor-pointer">{year} <TiArrowSortedDown /></span>
+                        <span className="flex items-center gap-2 cursor-pointer">{months[month].slice(0, 3)} <TiArrowSortedDown /></span>
+                    </div>
+                    {expenses && <ExpenseCalendar />}
+                </section>
+            </section>
         </HelmetProvider>
     );
 }
