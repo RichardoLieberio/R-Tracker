5ef7d41c3e2c02b079247a8a693ea6772af6809a
Richardo Lieberio <richardo.alt@gmail.com>
2025-01-02 15:39:39 +0700

    Added expense category in expense detail

--- src/components/ExpenseModal.jsx
+++ src/components/ExpenseModal.jsx
@@ -24,28 +24,27 @@ export default function ExpenseModal(props) {
                 <main className={`max-h-96 px-7 phone:px-8 flex flex-col gap-8 overflow-auto scrollbar-thin ${getScrollbarTrackBackground(theme)} ${getScrollbarThumbText(theme)}`}>
                     <section className="flex flex-col gap-8">
                         <header className="flex items-center justify-between text-xl font-semibold">Expense Detail</header>
-                        <main className="flex flex-col gap-4">
-                            <div className="mb-4 flex items-center gap-4">
-                                <div className="p-2 rounded-full shrink-0" style={{backgroundColor: `#${expense.category.color}`}}>
-                                    <img src={`${process.env.EXPENSE_CATEGORY_URI}/${expense.category.icon}`} alt={expense.category.name} className="w-6 h-6 shrink-0" />
+                        <main className="flex flex-col desktop:flex-row gap-8">
+                            <section className="flex desktop:flex-col items-center gap-4">
+                                <div className="p-4 rounded-full shrink-0" style={{backgroundColor: `#${expense?.category.color}`}}>
+                                    <img src={`${process.env.EXPENSE_CATEGORY_URI}/${expense?.category.icon}`} alt={expense?.category.name} className="w-8 h-8 desktop:w-16 desktop:h-16 shrink-0" />
+                                </div>
+                                <span className="desktop:w-20 text-center text-wrap break-words">{expense ? expense.category.name : '-'}</span>
+                            </section>
+                            <section className="desktop:flex-1 flex flex-col gap-4">
+                                <div className="relative flex flex-col">
+                                    <span className={`px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)}`}>Expense</span>
+                                    <span className={`px-3 py-2 text-wrap border ${getBorderNeutralColor(theme)} rounded-md break-words`}>{expense ? expense.expense : '-'}</span>
+                                </div>
+                                <div className="relative flex flex-col">
+                                    <span className={`px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)}`}>Amount</span>
+                                    <span className={`px-3 py-2 text-wrap border ${getBorderNeutralColor(theme)} rounded-md break-words`}>{expense ? expense.amount?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '-'}</span>
                                 </div>
                                 <div className="flex-1 relative flex flex-col">
-                                    <span className={`px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)}`}>Type</span>
-                                    <span className={`px-3 py-2 border ${getBorderNeutralColor(theme)} rounded-md break-words`}>{expense ? expense.category.name : '-'}</span>
+                                    <span className={`px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)}`}>Expense Date</span>
+                                    <span className={`px-3 py-2 text-wrap border ${getBorderNeutralColor(theme)} rounded-md break-words`}>{expense ? format(new Date(expense.expense_date), 'MMMM dd, yyyy') : '-'}</span>
                                 </div>
-                            </div>
-                            <div className="relative flex flex-col">
-                                <span className={`px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)}`}>Expense</span>
-                                <span className={`px-3 py-2 border ${getBorderNeutralColor(theme)} rounded-md break-words`}>{expense ? expense.expense : '-'}</span>
-                            </div>
-                            <div className="relative flex flex-col">
-                                <span className={`px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)}`}>Amount</span>
-                                <span className={`px-3 py-2 text-wrap border ${getBorderNeutralColor(theme)} rounded-md break-words`}>{expense ? expense.amount?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '-'}</span>
-                            </div>
-                            <div className="flex-1 relative flex flex-col">
-                                <span className={`px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)}`}>Expense Date</span>
-                                <span className={`px-3 py-2 text-wrap border ${getBorderNeutralColor(theme)} rounded-md break-words`}>{expense ? format(new Date(expense.expense_date), 'MMMM dd, yyyy') : '-'}</span>
-                            </div>
+                            </section>
                         </main>
                     </section>
                 </main>
