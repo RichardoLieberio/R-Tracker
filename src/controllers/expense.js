eed52444990f4b88549e232e57d2688f756aef25
Richardo Lieberio <richardo.alt@gmail.com>
2025-01-03 09:12:25 +0700

    Added controller to delete expense

--- src/controllers/expense.js
+++ src/controllers/expense.js
@@ -1,10 +1,10 @@
-// import {toast} from 'react-toastify';
+import {toast} from 'react-toastify';
 
 import axios from '../services/axios';
-// import {setToast} from '../services/toastService';
+import {setToast} from '../services/toastService';
 
 import store from '../redux/store';
-import {setExpenses, setExpenseCategories} from '../redux/dataSlice';
+import {setExpenses, removeExpense, setExpenseCategories} from '../redux/dataSlice';
 
 async function getExpenses(accessToken) {
     const config = {
@@ -51,4 +51,38 @@ async function getExpenseCategory(accessToken) {
     }
 }
 
-export default {getExpenses, getExpenseCategory};
+async function deleteExpense(id, csrfToken, accessToken, setModal) {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+            'CSRF-Token': csrfToken
+        },
+        authenticated: {
+            codes: [401],
+            route: '/login'
+        }
+    };
+
+    const response = await axios.delete(`/api/expense/${id}`, config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 200:
+            toast.success(response.data.msg);
+            store.dispatch(removeExpense(response.data.id));
+            store.getState().expensePage.expense._id === response.data.id && setModal(false);
+            break;
+        case 403:
+            setToast('error', response.data.msg);
+            location.reload();
+            break;
+        case 404:
+            toast.error(response.data.msg);
+            store.dispatch(removeExpense(id));
+            store.getState().expensePage.expense._id === id && setModal(false);
+            break;
+    }
+}
+
+export default {getExpenses, getExpenseCategory, deleteExpense};
