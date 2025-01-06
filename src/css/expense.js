25dca5e31a9f540f1744d9f2640783712c6d394f
Richardo Lieberio <richardo.alt@gmail.com>
2025-01-06 10:00:09 +0700

    Created css file for expense page

---
+++ src/css/expense.js
@@ -0,0 +1,14 @@
+import {getBackgroundColor, getDisabledBgNeutral30Color, getTextPrimaryColor, getTextHighlightColor, getTextErrorColor, getBorderNeutralColor, getFocusBorderPrimaryColor, getFocusBorderHighlightColor, getBorderErrorColor} from './color';
+
+export default function(theme) {
+    return {
+        labelMiddle: 'absolute left-3 bottom-1/2 translate-y-1/2 transition-transform cursor-text',
+        labelMiddleError: `absolute left-10 bottom-1/2 translate-y-1/2 ${getTextErrorColor(theme)} transition-transform cursor-text`,
+        labelTopBlur: `px-2 absolute left-1 -top-3 text-sm ${getBackgroundColor(theme)} transition-transform cursor-text`,
+        labelTopFocus: `px-2 absolute left-1 -top-3 text-sm ${theme === 'dark' ? getTextHighlightColor(theme) : getTextPrimaryColor(theme)} ${getBackgroundColor(theme)} transition-transform cursor-text`,
+        labelTopError: `px-2 absolute left-1 -top-3 text-sm ${getTextErrorColor(theme)} ${getBackgroundColor(theme)} transition-transform cursor-text`,
+        nonFocusInput: `w-full px-3 py-2 ${getBackgroundColor(theme)} border ${getBorderNeutralColor(theme)} rounded-md outline-none ${getDisabledBgNeutral30Color(theme)} disabled:cursor-not-allowed`,
+        defaultInput: `w-full px-3 py-2 ${getBackgroundColor(theme)} border ${getBorderNeutralColor(theme)} rounded-md outline-none ${theme === 'dark' ? getFocusBorderHighlightColor(theme) : getFocusBorderPrimaryColor(theme)} ${getDisabledBgNeutral30Color(theme)} disabled:cursor-not-allowed`,
+        defaultInputError: `w-full px-3 py-2 ${getBackgroundColor(theme)} pl-10 border ${getBorderErrorColor(theme)} rounded-md outline-none`
+    };
+};
