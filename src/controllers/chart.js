c18667e44096da0dc641506247b729adac726b41
Richardo Lieberio <richardo.alt@gmail.com>
2025-01-09 08:18:59 +0700

    Created controller for chart page

---
+++ src/controllers/chart.js
@@ -0,0 +1,51 @@
+import axios from '../services/axios';
+
+import store from '../redux/store';
+import {setExpenses, setExpenseCategories} from '../redux/dataSlice';
+
+async function getExpenses(accessToken) {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+        useAbortController: true,
+        authenticated: {
+            codes: [401],
+            route: '/login'
+        }
+    };
+
+    const response = await axios.get('/api/expense', config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 200:
+            store.dispatch(setExpenses(response.data.expenses));
+            break;
+    }
+}
+
+async function getExpenseCategory(accessToken) {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+        authenticated: {
+            codes: [401],
+            route: '/login'
+        }
+    };
+
+    const response = await axios.get('/api/expense/categories', config);
+    const status = response?.data?.status;
+
+    switch (status) {
+        case 200:
+            store.dispatch(setExpenseCategories(response.data.categories));
+            break;
+    }
+}
+
+export default {getExpenses, getExpenseCategory};
