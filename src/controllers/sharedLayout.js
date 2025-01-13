5f7dfa62ff4c8f361b0291d0829093bca31dbe19
Richardo Lieberio <richardo.alt@gmail.com>
2025-01-13 17:05:57 +0700

    Used logout reducer from expensePageSlice

--- src/controllers/sharedLayout.js
+++ src/controllers/sharedLayout.js
@@ -7,6 +7,7 @@ import store from '../redux/store';
 import {logout as authLogout} from '../redux/authSlice';
 import {logout as dataLogout} from '../redux/dataSlice';
 import {logout as expensePageLogout} from '../redux/expensePageSlice';
+import {logout as chartPageLogout} from '../redux/chartPageSlice';
 import {logout as userPageLogout} from '../redux/userPageSlice';
 import {logout as categoryPageLogout} from '../redux/categoryPageSlice';
 import {changeTheme as reduxChangeTheme} from '../redux/webSlice';
@@ -25,6 +26,7 @@ async function signout(navigate) {
             store.dispatch(authLogout());
             store.dispatch(dataLogout());
             store.dispatch(expensePageLogout());
+            store.dispatch(chartPageLogout());
             store.dispatch(userPageLogout());
             store.dispatch(categoryPageLogout());
 
