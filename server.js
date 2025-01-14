57b3f816633ec8fffed2061562b92a83992b42bb
Richardo Lieberio <richardo.alt@gmail.com>
2025-01-14 16:42:28 +0700

    Removed csurf from main middleware

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const csrf = require('csurf');
 
 const connectDB = require('./services/dbConnection');
 const sizeLimiter = require('./middlewares/sizeLimiter');
@@ -28,7 +27,6 @@ app.use(cors({
 app.use(cookieParser());
 app.use(sizeLimiter);
 app.use(express.json({limit: `${process.env.ACCEPT_JSON_MAX_SIZE_IN_MB}mb`}));
-app.use(csrf({cookie: true}));
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
